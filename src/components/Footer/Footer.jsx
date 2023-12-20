d22fc402c963295821238e93b8b4662f2e874636
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-20 19:23:50 +0200

    feat: Implement Footer component with copyright information
    Create the Footer component, providing a fixed layout at the bottom of the screen. Include copyright information for the powerlifting shop, styled with appropriate colors and borders.

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+
+
+    return (
+        <footer className="fixed bottom-0 flex items-center justify-center w-full h-16 bg-stone-800 border-solid border-1 border-stone-950">
+            <p className="flex w-max h-max text-stone-400">
+                © 2023 Your Powerlifting Shop Name. All Rights Reserved.
+            </p>
+        </footer>
+    )
+}
