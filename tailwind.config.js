db888e4399ea7efd575c9552214ce544c76579bd
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-20 19:20:24 +0200

    initialize a new project

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ['class', '[data-mode="dark"]'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+       'black-c': '#101314',
+       'blue-c':'#36454F'
+      },
+      backgroundImage:{
+        'fitnes-img': "url('./src/assets/background.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
+
