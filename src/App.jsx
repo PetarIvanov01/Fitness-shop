cf5daf8478d7b8e8dd482ee9a615799e8abaca3d
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-20 19:21:52 +0200

    style: Add background styling and structure to App component
    Enhance the App component by adding background styling with a fitness image, and structure it with the inclusion of Header and Footer components for a cohesive layout.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Headers/MainHeader";
+
+export default function App() {
+
+
+    return (
+        <main className="h-screen bg-fitnes-img bg-cover bg-no-repeat bg-center bg-opacity-85">
+
+            <Header />
+
+
+            <Footer />
+
+        </main>
+    )
+}
