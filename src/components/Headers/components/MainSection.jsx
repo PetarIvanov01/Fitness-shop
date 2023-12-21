ca240a6d3d9a7c5851f364fa30871a3a8492db18
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-21 18:10:47 +0200

    style: Update MainSection component with a logo
    Add a logo to the MainSection component. Adjust styling to include the logo beside the menu button, centered horizontally.

--- src/components/Headers/components/MainSection.jsx
+++ src/components/Headers/components/MainSection.jsx
@@ -1,15 +1,20 @@
-import menu from "../../../assets/menu.svg"
+import menu from "../../../assets/menu.svg";
+import logo from "../../../assets/logo.jpg";
 
 export default function MainSection({ handleAsideVisible }) {
 
 
     return (
-        <section className="flex items-center px-6 bg-stone-900 bg-opacity-95 h-16">
+        <section className="flex items-center px-6 bg-stone-800 bg-opacity-95 h-16">
 
             <button onClick={handleAsideVisible} className="w-8 hover:scale-110">
-                <img className="w-auto " src={menu} alt="menu" />
+                <img className="w-auto" src={menu} alt="menu" />
             </button>
 
+            <div className="cursor-pointer w-16  absolute left-1/2 transform -translate-x-1/2">
+                <img src={logo} alt="logo" className="h-full" />
+            </div>
+
         </section>
-    )
-}
+    );
+};
