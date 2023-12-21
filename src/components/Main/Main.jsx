c7e795c88a6e03e5c32d7c2f73f2facca56ac4de
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-21 18:08:07 +0200

    feat: Integrate TopOffers component into Main component
    Add the TopOffers component to the Main component to display top offers. Include a placeholder for future content within the article section.

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,22 @@
+import TopOffers from "./TopOffer/TopOffer";
+
+export default function Main() {
+
+
+    return (
+        <main className="h-full">
+            <section className="flex px-2 flex-col min-h-12 ">
+
+                <TopOffers />
+                <hr className="my-4 border-t-2 border-black" />
+
+                <article>
+                    
+
+                </article>
+
+            </section>
+        </main>
+    );
+};
+
