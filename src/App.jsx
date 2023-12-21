1859d3575f3698dca794a74d6efe75dfc2c0475c
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-21 18:06:15 +0200

    Adding google fonts and colors to tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Headers/MainHeader";
+import Main from "./components/Main/Main";
 
 export default function App() {
-
+    // 
 
     return (
-        <main className="h-screen bg-fitnes-img bg-cover bg-no-repeat bg-center bg-opacity-85">
+        <div className="h-screen overflow-auto bg-fitnes-img bg-cover bg-no-repeat bg-center">
 
             <Header />
 
+            <Main />
 
             <Footer />
 
-        </main>
+        </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'sans': ['"Roboto","sans-serif"'],
+    },
     extend: {
-      colors:{
-       'black-c': '#101314',
-       'blue-c':'#36454F'
+      colors: {
+        'black-c': '#101314',
+        'blue-c': '#36454F'
       },
-      backgroundImage:{
+      backgroundImage: {
         'fitnes-img': "url('./src/assets/background.jpg')"
       }
     },
