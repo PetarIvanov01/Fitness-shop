fda9259da5a0682ea16406cf90404e4a6e901b28
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-26 23:37:07 +0200

    feat: Implement Categories component with Cards

---
+++ src/components/Main/EquipCategories/components/Cards.jsx
@@ -0,0 +1,22 @@
+export default function Card({ id, imgPath, alt, buttonText }) {
+
+    // TODO: Redirect to the particular page
+
+    return (
+        <div className="size-60 max-md:size-36 
+        relative overflow-hidden hover:scale-105 
+        transition-transform duration-300">
+
+            <img className="w-full h-full object-cover 
+            transition-transform duration-300 transform hover:scale-110"
+                src={imgPath} alt={alt} />
+
+            <a href={`item/${id}`} className="
+            absolute bottom-5 left-1/2 transform -translate-x-1/2
+            transition-colors duration-300 max-md:text-[0.8em] 
+            text-center bg-black text-white font-bold px-3 py-2 hover:text-gray-300 ">
+                {buttonText}
+            </a>
+        </div>
+    )
+}
---
+++ src/components/Main/EquipCategories/components/Categories.jsx
@@ -0,0 +1,16 @@
+import Card from "./Cards";
+import mockData from "../../../../assets/utils/equipData.json";
+import resolveImgPath from "../../../../utils/resolveImgPaths";
+
+export default function Categories() {
+
+    const resolvedImgData = resolveImgPath(mockData); 
+
+    return (
+        <article className="mx-2 mb-4 mt-2 flex flex-wrap gap-14 max-md:gap-4 justify-center">
+
+            {resolvedImgData.map((props) => <Card key={props.id} {...props} />)}
+
+        </article>
+    )
+}
