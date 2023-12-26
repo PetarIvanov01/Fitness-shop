8269692c7aa40107aca3099780d116eb1f482b6f
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-26 23:30:42 +0200

    Added animation styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,43 @@ export default {
       'sans': ['"Roboto","sans-serif"'],
     },
     extend: {
+      keyframes: {
+        'fade-in': {
+          "0%": {
+            opacity: 0
+          },
+          "100%": {
+            opacity: 1
+          }
+        },
+        'fade-out': {
+          "0%": {
+            opacity: 1
+          },
+          "100%": {
+            opacity: 0
+          }
+        },
+        'slide-up': {
+          "0%": {
+            transform: "translateX(-50%)"
+          },
+          "-50%": {
+            transform: "translateX(0%)"
+          }
+        }
+      },
+      animation: {
+        'fade-in': 'fade-in 1000ms forwards' ,
+        'fade-out': 'fade-out 1000ms forwards',
+        'slide-up': 'slide-up 1500ms forwards',
+      },
       colors: {
         'black-c': '#101314',
         'blue-c': '#36454F'
       },
       backgroundImage: {
-        'fitnes-img': "url('./src/assets/background.jpg')"
+        'fitnes-img': "url('/src/assets/background.jpg')"
       }
     },
   },
