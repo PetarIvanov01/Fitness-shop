3c0a9564f2fa2308b7c0261b3085bdd60f8f3638
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-26 19:42:23 +0200

    Creating Router component which will hold all route paths with their components

---
+++ src/components/Routes/Router.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom"
+import Main from "../Main/Main"
+
+export default function Router() {
+
+
+    return (
+        <Routes>
+            <Route path="/" element={<Main />} />
+        </Routes>
+    )
+}
