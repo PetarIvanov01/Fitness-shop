c94949a6182bdc787c32192bfc5319e8c42ecb03
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-26 23:30:25 +0200

    Refactor: Removed the Main component, and added the Router one.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Headers/MainHeader";
-import Main from "./components/Main/Main";
+import Router from "./components/Routes/Router";
 
 export default function App() {
-    // 
 
     return (
-        <div className="h-screen overflow-auto bg-fitnes-img bg-cover bg-no-repeat bg-center">
+        <div className="min-h-screen flex flex-col bg-fitnes-img bg-cover bg-no-repeat bg-center">
 
             <Header />
 
-            <Main />
+            <Router />
 
             <Footer />
 
         </div>
-    )
-}
+    );
+};
