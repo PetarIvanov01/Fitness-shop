d2e43aff03d55b1b7789437cba2a82814de9db54
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-26 23:33:56 +0200

    Refactor: wrapped the img tag with div, and added styles to improve the UI

--- src/components/Main/TopOffer/components/ControlOfferButton.jsx
+++ src/components/Main/TopOffer/components/ControlOfferButton.jsx
@@ -1,4 +1,10 @@
 export default function ControlOfferButton({ icon, alt, handler }) {
 
-    return <img onClick={handler} className="cursor-pointer max-sm:size-12" src={icon} alt={alt} />
+    return (
+        <div className="max-sm:size-12 rounded-full flex justify-center items-center mx-2 border min-h-12 min-w-8
+         hover:scale-105 hover:opacity-40">
+            <img onClick={handler} className="cursor-pointer size-full" src={icon} alt={alt} />
+        </div>
+    )
+
 };
