1d763729dbec71dc92311086762fe0ba081329c1
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-28 00:10:32 +0200

    feat: Update MainSection and component with TODO: conditional login/logout link

--- src/components/Headers/AsideHeader.jsx
+++ src/components/Headers/AsideHeader.jsx
@@ -1,19 +1,43 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { IoMdLogIn } from "react-icons/io";
+import { BiLogOutCircle } from "react-icons/bi"
+
+/*
+ Todo: Depending on the user status show logout button;
+*/
+const categories = ['Cardio', 'Machines', 'Free Weights'];
+
 export default function AsideHeader({ visible }) {
 
+    const [isVisible, setVisible] = useState(false);
+
+    const handleVisibility = () => {
+        setVisible(prev => !prev);
+    };
 
     return (
         <aside className={`${visible ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-[-100%]'}
-             absolute bg-opacity-95 transition-transform w-52 px-8 py-16 bg-stone-900 text-stone-50`}>
+            absolute flex items-center flex-col h-96 
+            transition-transform w-52 px-8 py-12 bg-stone-950 text-stone-50
+            max-sm:w-32 z-10`}>
 
-            <h2 className="mb-8 font-bold  uppercase md:text-xl text-slate-300 ">
+            <h2 className="mb-8 font-bold uppercase max-sm:text-xs md:text-xl text-slate-300 ">
                 Products
             </h2>
 
-            <div>
-                <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-300 hover:bg-stone-500">
-                    + Categories
-                </button>
-            </div>
+            <button onClick={handleVisibility} className="px-4 py-2 mb-2 text-xs md:text-base max-sm:px-2 rounded-md bg-stone-700 text-stone-300 hover:bg-stone-500">
+                + Categories
+            </button>
+
+            <ul className={`${!isVisible && "hidden"} flex flex-col pl-4 pt-5 gap-2 w-full`}>
+                {categories.map(e => <li className="opacity-50 w-auto cursor-pointer hover:opacity-100" key={e}> {e} </li>)}
+            </ul>
+
+            <Link to={'/login'}>
+                <IoMdLogIn className="absolute bottom-5 right-5 size-6 cursor-pointer" />
+            </Link>
+
         </aside>
-    )
-}
+    );
+};
--- src/components/Headers/components/MainSection.jsx
+++ src/components/Headers/components/MainSection.jsx
@@ -1,19 +1,27 @@
+import { IoMdLogIn } from "react-icons/io";
+import { BiLogOutCircle } from "react-icons/bi"
 import menu from "../../../assets/menu.svg";
 import logo from "../../../assets/logo.png";
-
+import { Link } from "react-router-dom";
+/*
+Todo: Depending on the user status show the right Icon(login/logout)
+*/
 export default function MainSection({ handleAsideVisible }) {
 
-
     return (
-        <section className="flex items-center px-6  bg-[#4e4242] opacity-95 h-16">
+        <section className="flex items-center px-6 bg-[#4e4242] opacity-95 h-16">
 
             <button onClick={handleAsideVisible} className="w-8 hover:scale-110">
                 <img className="w-auto" src={menu} alt="menu" />
             </button>
 
             <div className="cursor-pointer w-16 absolute left-1/2 transform -translate-x-1/2">
-                <img src={logo} alt="logo" className="h-full" />
+                <Link to="/"><img src={logo} alt="logo" className="h-full" /></Link>
             </div>
+
+            <Link to={'/login'} className="block ml-auto size-8">
+                <IoMdLogIn className="size-full" />
+            </Link>
 
         </section>
     );
