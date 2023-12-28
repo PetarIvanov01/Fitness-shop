717e82a2ee688f9164e8789c16951d4377855749
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-28 15:29:16 +0200

    Update: Adding new register route

--- src/components/Routes/Router.jsx
+++ src/components/Routes/Router.jsx
@@ -4,11 +4,11 @@ import AuthenticationSection from "../Authentication/AuthenticationSection"
 
 export default function Router() {
 
-
     return (
         <Routes>
             <Route path="/" element={<Main />} />
             <Route path="/login" element={<AuthenticationSection />} />
+            <Route path="/register" element={<AuthenticationSection />} />
         </Routes>
     )
 }
