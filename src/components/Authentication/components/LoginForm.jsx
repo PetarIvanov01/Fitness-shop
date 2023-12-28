f5ed212f7884a151ec5750c728e74086102ff0e9
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-28 15:33:23 +0200

    refactor: Update LoginForm component to use useForm hook

--- src/components/Authentication/components/LoginForm.jsx
+++ src/components/Authentication/components/LoginForm.jsx
@@ -2,25 +2,46 @@ import Label from "./Tags/Label";
 import Input from "./Tags/Input";
 import Button from "./Tags/Button";
 import Heading from "./Tags/Heading";
+import useForm from "../../../hooks/useForm";
 
+const initialState = {
+    email: '',
+    password: ''
+}
 export default function LoginForm() {
 
+    const onSubmit = (values) => {
+        //Todo add submit logic for login
+    }
+
+    const { values, handleChangeValues, handleSubmitForm } = useForm(initialState, onSubmit);
 
     return (
         <div className="w-1/2 px-6 flex flex-col items-center text-white ">
 
             <Heading text={'Registered User'} />
 
-            <form className="flex flex-col w-full gap-2 font-alegreya font-medium">
+            <form onSubmit={handleSubmitForm} className="flex flex-col w-full gap-2 font-alegreya font-medium">
 
                 <div className="w-full">
                     <Label htmlFor={'email'} text={'Email address'} />
-                    <Input id={'email'} name={'email'} placeholder={'Email address...'} type={'email'} />
+                    <Input handler={handleChangeValues}
+                        value={values.email}
+                        id={'email'}
+                        name={'email'}
+                        placeholder={'Email address...'}
+                        type={'email'}
+                        title="Please provide valid email address" />
                 </div>
 
                 <div className="w-full">
                     <Label htmlFor={'password'} text={'Password'} />
-                    <Input id={'password'} name={'password'} placeholder={'Password...'} type={'password'} />
+                    <Input handler={handleChangeValues}
+                        value={values.password}
+                        id={'password'}
+                        name={'password'}
+                        placeholder={'Password...'}
+                        type={'password'} />
                 </div>
 
                 <Button text={'Sign In'} />
