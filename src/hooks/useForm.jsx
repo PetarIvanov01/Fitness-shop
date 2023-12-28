5f3448baa8702556bf6e825137697427bf6c146e
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-28 15:30:00 +0200

    New: Create custom hook to handle input changes and submiting the Form

---
+++ src/hooks/useForm.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+export default function useForm(
+    intialState = {},
+    onSubmit
+) {
+    const [values, setValues] = useState(intialState);
+
+    const handleChangeValues = (e) => {
+        setValues(prev => ({
+            ...prev,
+            [e.target.name]: e.target.value
+        }));
+    };
+
+    const handleSubmitForm = (e) => {
+        e.preventDefault();
+        onSubmit(values);
+    };
+
+    return {
+        values,
+        handleChangeValues,
+        handleSubmitForm
+    }
+}
