a1df2e0607e76b19241a48cae6c53e84ac9dd75e
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-28 15:40:02 +0200

    refactor: Update GetStarted component to be more flexible

--- src/components/Authentication/components/GetStarted.jsx
+++ src/components/Authentication/components/GetStarted.jsx
@@ -1,10 +1,13 @@
 import { GrLinkNext } from "react-icons/gr";
 import Heading from "./Tags/Heading";
 
-export default function GetStarted() {
+export default function GetStarted({ handler, isGuest }) {
+
+    const message = isGuest ? "Already an user" : "Get Started";
+    const width = isGuest ? "w-full" : "w-1/2";
 
     return (
-        <div className="w-1/2 text-white ">
+        <div className={`${width} text-white`}>
 
             <Heading text={'For new users'} />
 
@@ -13,8 +16,8 @@ export default function GetStarted() {
                 <p>Register to unlock buying capabilities, add new items to the shop, and enjoy other exclusive features. Elevate your fitness shopping experience today!</p>
             </div>
 
-            <button className="flex opacity-55 items-center mt-6 text-sm bg-white py-1 px-1 rounded-sm hover:scale-105 hover:opacity-85 text-black">
-                Get Started <GrLinkNext className="ml-2" />
+            <button onClick={handler} className="flex gap-2 opacity-55 items-center mt-6 text-sm bg-white py-1 px-1 rounded-sm hover:scale-105 hover:opacity-85 text-black">
+                {message} <GrLinkNext />
             </button>
         </div>
     );
