3b4966465c8b85aa744fafc6e9378645624d4bdc
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-31 20:16:34 +0200

    Update: Added catalog route to dispaly catalog data

--- src/components/Routes/Router.jsx
+++ src/components/Routes/Router.jsx
@@ -1,14 +1,19 @@
-import { Routes, Route } from "react-router-dom"
-import Main from "../Main/Main"
-import AuthenticationSection from "../Authentication/AuthenticationSection"
+import { Routes, Route } from "react-router-dom";
+import Main from "../Main/Main";
+import AuthenticationSection from "../Authentication/AuthenticationSection";
+import Catalog from "../Catalog/Catalog";
 
 export default function Router() {
 
     return (
-        <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/login" element={<AuthenticationSection />} />
-            <Route path="/register" element={<AuthenticationSection />} />
-        </Routes>
-    )
-}
+        <div className="flex-1 overflow-auto py-6">
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/login" element={<AuthenticationSection />} />
+                <Route path="/register" element={<AuthenticationSection />} />
+
+                <Route path="/catalog" element={<Catalog />} />
+            </Routes>
+        </div>
+    );
+};
