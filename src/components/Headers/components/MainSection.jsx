b0f9b1603c681fd09c4f89ee9d8d469ff360a4e5
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-08 21:55:55 +0200

    update: Extracting data from the store, and conditionaly render login and logout button

--- src/components/Headers/components/MainSection.jsx
+++ src/components/Headers/components/MainSection.jsx
@@ -3,11 +3,14 @@ import { BiLogOutCircle } from "react-icons/bi"
 import menu from "../../../assets/menu.svg";
 import logo from "../../../assets/logo.png";
 import { Link } from "react-router-dom";
+import useStore from "../../../zustand/store";
 /*
 Todo: Depending on the user status show the right Icon(login/logout)
 */
 export default function MainSection({ handleAsideVisible }) {
 
+    const user = useStore((state) => state.user);
+    
     return (
         <section className="flex items-center px-6 bg-[#4e4242] opacity-95 h-16">
 
@@ -19,10 +22,15 @@ export default function MainSection({ handleAsideVisible }) {
                 <Link to="/"><img src={logo} alt="logo" className="h-full" /></Link>
             </div>
 
-            <Link to={'/login'} className="block ml-auto size-8">
-                <IoMdLogIn className="size-full" />
-            </Link>
-
+            {user ?
+                <Link to={'/logout'} className="block ml-auto size-8">
+                    <BiLogOutCircle className="size-full" />
+                </Link>
+                :
+                <Link to={'/login'} className="block ml-auto size-8">
+                    <IoMdLogIn className="size-full" />
+                </Link>
+            }
         </section>
     );
 }; 
