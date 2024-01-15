7c2c7a15348348a4fd587f1d519d62967082e5d0
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-15 21:56:00 +0200

    Implemented these guards

--- src/components/Routes/Router.jsx
+++ src/components/Routes/Router.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Main from "../Main/Main";
 import AuthenticationSection from "../Authentication/AuthenticationSection";
 import Catalog from "../Catalog/Catalog";
+import { IsGuest, IsAuthenticatedUser } from "./Guards/AuthGuard";
+import Logout from "../Authentication/components/Logout";
 
 export default function Router() {
 
@@ -9,8 +11,15 @@ export default function Router() {
         <div className="flex-1 overflow-auto py-6">
             <Routes>
                 <Route path="/" element={<Main />} />
-                <Route path="/login" element={<AuthenticationSection />} />
-                <Route path="/register" element={<AuthenticationSection />} />
+
+                <Route element={<IsGuest />}>
+                    <Route path="/login" element={<AuthenticationSection />} />
+                    <Route path="/register" element={<AuthenticationSection />} />
+                </Route>
+
+                <Route element={<IsAuthenticatedUser />}>
+                    <Route path="/logout" element={<Logout />} />
+                </Route>
 
                 <Route path="/catalog" element={<Catalog />} />
             </Routes>
