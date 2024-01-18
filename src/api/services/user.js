9220a12c178fac730fea54dce4bcf5cebe97a8d4
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-18 21:47:55 +0200

    Update: Added "sendUserLogout function which clears the user store and remove the infromation from the browser storage

--- src/api/services/user.js
+++ src/api/services/user.js
@@ -1,10 +1,11 @@
 import { setUserInStore } from "../../zustand/authExternal";
 import * as methods from "../requester"
-import { setToBrowserStorage } from "./storage";
+import { removeFromBrowserStorage, setToBrowserStorage } from "./storage";
 
 const endpoints = {
     register: '/user/sign-up',
-    login: '/user/sign-in'
+    login: '/user/sign-in',
+    logout: '/user/logout'
 }
 
 export const sendUserRegistration = async (userData) => {
@@ -18,6 +19,15 @@ export const sendUserLogin = async (userData) => {
 
     syncUserState(user);
     return null;
+}
+
+export const sendUserLogout = async () => {
+    /* 
+    TODO: send logout req when its ready on the server 
+     await methods.get(endpoints.logout);
+    */
+    setUserInStore(null);
+    return removeFromBrowserStorage('user');
 }
 
 function syncUserState(user) {
