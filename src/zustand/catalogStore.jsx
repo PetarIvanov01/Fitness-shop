c817e360ea21f71e3e03086998847e71ba3088c7
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-22 17:47:57 +0200

    feat: Implement catalogSlice for the zustand Store with fetch method to fetch catalog items from the API

---
+++ src/zustand/catalogStore.jsx
@@ -0,0 +1,19 @@
+import { getCatalog } from "../api/services/catalog";
+
+const catalogSlice = (set) => ({
+    items: [],
+    isLoading: true,
+    fetch: async (category, signal) => {
+        try {
+            const data = await getCatalog(category, signal);
+            set({ items: data.values, isLoading: false })
+        }
+        catch (error) {
+            // Todo implement error handling 
+            console.log(error);
+            throw error;
+        }
+    }
+});
+
+export default catalogSlice;
