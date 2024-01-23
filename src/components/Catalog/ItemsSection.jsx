0d0f8f68c73d6b68831ae633e750d44930584347
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-23 22:17:39 +0200

    feat: Add cart functionality to ItemsSection and integrate with Zustand store, displaying toast message after adding to cart

--- src/components/Catalog/ItemsSection.jsx
+++ src/components/Catalog/ItemsSection.jsx
@@ -1,11 +1,37 @@
 import Card from "./components/Card";
+import useStore from "../../zustand/store";
+import { toast } from "sonner";
+import { useNavigate } from "react-router-dom";
 
 export default function ItemsSection({ data }) {
+
+    const navigate = useNavigate();
+    const addCartDataToCookies = useStore((state) => state.addCartDataToCookies);
+    
+    const onClickAddCookieData = ({ id, name }) => {
+        return () => {
+            addCartDataToCookies(id);
+            toast.success(`Item-${name}: Added to Cart`, {
+                position: "top-right",
+                duration: 2000,
+                action: {
+                    label: 'Go to Cart',
+                    onClick: () => {
+                        navigate('/cart');
+                    },
+                },
+            });
+        }
+    }
 
     return (
         <section className="flex justify-center flex-wrap gap-6 pt-5 overflow-auto">
 
-            {data?.map((products) => <Card key={products.product_id} {...products} />)}
+            {data?.map((products) => <Card
+                key={products.product_id}
+                {...products}
+                onClickAddCookieData={onClickAddCookieData({ id: products.product_id, name: products.title })}
+            />)}
 
         </section>
     );
