01a82b1a686daed80f4c22ac1c29c897866d62c0
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-23 22:13:48 +0200

    refactor: Combine multiple Zustand stores into a single store

--- src/zustand/store.jsx
+++ src/zustand/store.jsx
@@ -1,12 +1,15 @@
 import { create } from "zustand";
-import { devtools } from "zustand/middleware"
-import { getFromBrowserStorage } from "../api/services/storage";
+import { devtools } from "zustand/middleware";
+
+import userSlice from "./userStore";
+import catalogSlice from "./catalogStore";
+import cartStore from "./cartStore";
 
 const store = devtools((set) => ({
-    user: getFromBrowserStorage('user')
+    ...userSlice(set),
+    ...catalogSlice(set),
+    ...cartStore(set)
 }));
 
 const useStore = create(store);
 export default useStore;
-
-
