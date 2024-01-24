462e77ec3f5499ae99c301078b69513cdd417bee
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-24 21:57:52 +0200

    refactor: Update TableCart to use zustand for  cart data fetching and mapping through all cart products

--- src/components/Cart/components/Table/CartTable.jsx
+++ src/components/Cart/components/Table/CartTable.jsx
@@ -1,7 +1,23 @@
+import { useEffect } from "react";
+import useStore from "../../../../zustand/store";
 import TableHead from "./TableHead";
 import TableRow from "./TableRow";
 
 export default function TableCart() {
+
+    const cartItems = useStore((state) => state.cart);
+    const fetchCartData = useStore((state) => state.fetchCartData);
+
+    useEffect(() => {
+        const abortController = new AbortController();
+
+        fetchCartData(abortController.signal);
+
+        return () => {
+            abortController.abort();
+        }
+    }, [])
+
     return (
         <div className="grow px-4">
             <table className="w-full border-collapse border border-gray-300">
@@ -9,8 +25,7 @@ export default function TableCart() {
                 <TableHead />
 
                 <tbody>
-                    <TableRow />
-                    <TableRow />
+                    {cartItems.map(e => <TableRow key={e.product_id}  {...e} />)}
                 </tbody>
 
             </table>
