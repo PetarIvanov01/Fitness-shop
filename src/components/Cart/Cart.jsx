84a69f0df70db4dad5874db16d3317b8a21b573a
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-24 22:05:48 +0200

    refactor: Update Cart component to use user state from zustand store

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,20 +1,20 @@
-import { Link, useOutletContext } from "react-router-dom";
+import { Link } from "react-router-dom";
+import useStore from "../../zustand/store";
 
 import TableCart from "./components/Table/CartTable";
 import CheckoutSection from "./components/CheckoutSection";
 
 export default function Cart() {
 
-    const context = useOutletContext();
+    const user = useStore((state) => state.user);
 
     return (
-
         <div className="h-auto font-alegreya">
             <header className="w-full bg-slate-800 opacity-90 p-4 flex flex-col items-center gap-2 text-center">
-                {context.hasUser ? (
+                {user.id ? (
                     <p className="text-green-500 ">
-                    Welcome back, Petar! Ready to make your purchase?
-                </p>
+                        Welcome back, Petar! Ready to make your purchase?
+                    </p>
                 ) : (
                     <p className="text-orange-300">
                         You don't have an account, and the purchase cannot be performed.
@@ -33,7 +33,7 @@ export default function Cart() {
                     <h2 className="text-2xl font-bold mb-4">Continue Shopping</h2>
                     <p className="text-gray-300">
                         Explore more fitness equipment and accessories.
-                        <Link to={'/shop'} className="underline ml-1 hover:no-underline">Go back to shopping</Link>.
+                        <Link to={'/catalog'} className="underline ml-1 hover:no-underline">Go back to shopping</Link>.
                     </p>
                 </div>
 
