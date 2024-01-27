8d2a2d46d5caf5818970fbf249a15450d7ddd5d0
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-27 19:09:40 +0200

    Modified the ProductView component to utilize React Router's useParams hook to extract the productId from the route parameters. Integrated the fetchProduct function from the store to fetch product details based on the extracted productId.

--- src/components/Product/ProductView.jsx
+++ src/components/Product/ProductView.jsx
@@ -1,7 +1,24 @@
+import { useParams } from 'react-router-dom';
 import ProductInformation from './components/ProductInformation';
 import RelatedProducts from './components/RelatedProducts';
+import useStore from '../../zustand/store';
+import { useEffect } from 'react';
 
 export default function ProductView() {
+    const { productId } = useParams();
+    const fetchProduct = useStore((state) => state.fetchProduct);
+
+    useEffect(() => {
+        const abortController = new AbortController();
+
+        if (productId) {
+            fetchProduct(productId, abortController.signal);
+        }
+
+        return () => {
+            abortController.abort();
+        };
+    }, [productId]);
     return (
         <div className="mx-1 min-h-full px-3 font-alegreya">
             <section className="flex w-full flex-col rounded-lg bg-gray-900 p-4 text-white opacity-95">
