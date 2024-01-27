844445e72c39e85748f6244e5e241cd6ae049da5
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-27 19:08:41 +0200

    Updated the ProductInformation component to dynamically display product details fetched from the store. Utilized the resolveServerImg utility function for consistent image resolution.

--- src/components/Product/components/ProductInformation.jsx
+++ src/components/Product/components/ProductInformation.jsx
@@ -1,46 +1,47 @@
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import useStore from '../../../zustand/store';
-import { useEffect } from 'react';
+import resolveServerImg from '../../../utils/resolveServerImg';
+import { toast } from 'sonner';
 
 export default function ProductInformation() {
-    const { productId } = useParams();
+    const navigate = useNavigate();
 
     const product = useStore((state) => state.product);
-    const fetchProduct = useStore((state) => state.fetchProduct);
+    const addCartDataToCookies = useStore(
+        (state) => state.addCartDataToCookies
+    );
 
-    useEffect(() => {
-        const abortController = new AbortController();
+    const image = resolveServerImg(product.image);
 
-        if (productId) {
-            fetchProduct(productId, abortController.signal);
-        }
-
-        return () => {
-            abortController.abort();
-        };
-    }, []);
+    const onAddToCartHandler = () => {
+        addCartDataToCookies(product.product_id);
+        toast.success(`Item-${product.title}: Added to Cart`, {
+            position: 'top-right',
+            duration: 2000,
+            action: {
+                label: 'Go to Cart',
+                onClick: () => {
+                    navigate('/cart');
+                },
+            },
+        });
+    };
 
     return (
         <div className="flex w-full flex-col">
             <div className="flex flex-col items-center gap-2">
-                <h1 className="pb-2 text-2xl font-bold">
-                    Horizontal Bench Classic Series by Active Gym
-                </h1>
+                <h1 className="pb-2 text-2xl font-bold">{product.title}</h1>
 
                 <div className="max-w-96 pb-4">
                     <img
-                        src="https://pulsegymshop.bg/image/cache/catalog/products/active-gym/s368-(1)-black-621x424h.png"
-                        alt="Horizontal Bench Classic Series by Active Gym"
+                        src={image}
+                        alt={product.title}
                         className="w-full rounded-lg bg-white shadow-lg"
                     />
                 </div>
             </div>
 
-            <p className="pb-4 text-lg">
-                The Horizontal Bench from the Classic Series by Active Gym is a
-                multifunctional bench for full-body workouts using weights,
-                dumbbells, or body weight.
-            </p>
+            <p className="pb-4 text-lg">{product.description}</p>
 
             <ul className="list-disc pb-4 pl-6">
                 <li>
@@ -52,8 +53,11 @@ export default function ProductInformation() {
             </ul>
 
             <div className="flex w-fit flex-col justify-center self-end text-xl font-bold max-sm:text-[1em]">
-                <div className="pb-2">Price: $1,500.00</div>
-                <button className="rounded-md bg-blue-500 px-4 py-2 text-white ">
+                <div className="pb-2">Price: ${product.price}</div>
+                <button
+                    onClick={onAddToCartHandler}
+                    className="rounded-md bg-blue-500 px-4 py-2 text-white "
+                >
                     Add to Cart
                 </button>
             </div>
