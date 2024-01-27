fb58f1fc6e67c84a6a989709c455c913ce4fb19d
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-27 19:06:57 +0200

    Extract the CartHeader to own component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,30 +1,13 @@
 import { Link } from 'react-router-dom';
-import useStore from '../../zustand/store';
 
 import TableCart from './components/Table/CartTable';
 import CheckoutSection from './components/CheckoutSection';
+import CartHeader from './components/Table/CartHeader';
 
 export default function Cart() {
-    const user = useStore((state) => state.user);
-
     return (
         <div className="h-auto font-alegreya">
-            <header className="flex w-full flex-col items-center gap-2 bg-slate-800 p-4 text-center opacity-90">
-                {user.id ? (
-                    <p className="text-green-500 ">
-                        Welcome back, Petar! Ready to make your purchase?
-                    </p>
-                ) : (
-                    <p className="text-orange-300">
-                        You don't have an account, and the purchase cannot be
-                        performed. Please{' '}
-                        <Link to={'/login'} className="underline">
-                            Log In
-                        </Link>
-                        .
-                    </p>
-                )}
-            </header>
+            <CartHeader />
 
             <section className="flex max-h-full flex-col gap-8 bg-slate-800 text-white opacity-95">
                 <TableCart />
