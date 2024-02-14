3d9a7fd25f66b3bc663a99d6b9e5c6711b3e0bcb
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-14 19:51:41 +0200

    clear cart data on user logout

--- src/components/Authentication/components/Logout.jsx
+++ src/components/Authentication/components/Logout.jsx
@@ -2,10 +2,16 @@ import { useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 import { sendUserLogout } from '../../../api/services/user';
 import { toast } from 'sonner';
+import useStore from '../../../zustand/store';
 
 export default function Logout() {
+    const clearCartData = useStore((state) => state.clearCartData);
+
     useEffect(() => {
-        sendUserLogout().then((e) => toast('You are logged out!'));
+        sendUserLogout().then((e) => {
+            clearCartData();
+            toast('You are logged out!');
+        });
     }, []);
 
     return <Navigate to={'/'} />;
