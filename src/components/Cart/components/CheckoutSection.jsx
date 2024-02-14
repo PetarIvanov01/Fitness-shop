e266f5b3395ed0e32ae671f92cdc021babbcb017
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-14 19:52:37 +0200

    improve checkout button styling and behavior

--- src/components/Cart/components/CheckoutSection.jsx
+++ src/components/Cart/components/CheckoutSection.jsx
@@ -4,8 +4,13 @@ import { BsFillCartXFill } from 'react-icons/bs';
 import useStore from '../../../zustand/store';
 
 export default function CheckoutSection() {
+    const user = useStore((state) => state.user);
     const cart = useStore((state) => state.cart);
     const clearCartData = useStore((state) => state.clearCartData);
+
+    const styleForUserCheckout = user
+        ? 'bg-green-500 hover:bg-green-600'
+        : 'bg-slate-500 cursor-not-allowed';
 
     const totalPrice = useMemo(() => {
         if (cart.length === 0) return 0;
@@ -21,7 +26,9 @@ export default function CheckoutSection() {
                 <p className="text-lg font-bold text-white">
                     Total Price: $ {totalPrice.toFixed(2)}
                 </p>
-                <button className="flex items-center justify-center gap-2 rounded-md bg-green-500 px-4 py-2 text-white transition duration-300 hover:bg-green-600">
+                <button
+                    className={`flex ${styleForUserCheckout} items-center justify-center gap-2 rounded-md px-4 py-2 text-white transition duration-300 `}
+                >
                     Checkout <GrLinkNext />
                 </button>
                 <button
