74206d5192e0739119ba48dec418a9ae514d9ade
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-15 21:12:44 +0200

    feat: Enhance MainSection component with user and cart functionalities

--- src/components/Headers/components/MainSection.jsx
+++ src/components/Headers/components/MainSection.jsx
@@ -1,16 +1,18 @@
-import { IoMdLogIn } from 'react-icons/io';
 import { BiLogOutCircle } from 'react-icons/bi';
 import menu from '../../../assets/menu.svg';
 import logo from '../../../assets/logo.png';
+import { FaRegUserCircle, FaShoppingCart } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import useStore from '../../../zustand/store';
 
 export default function MainSection({
     handleAsideVisible,
     onClickClose,
     user,
 }) {
+    const cartLenght = useStore((state) => state.length);
     return (
-        <section className="flex h-16 items-center bg-[#4e4242] px-6 opacity-95">
+        <section className="flex h-16 items-center justify-between bg-[#4e4242] px-6 opacity-95">
             <button
                 onClick={handleAsideVisible}
                 className="w-8 hover:scale-110"
@@ -24,23 +26,25 @@ export default function MainSection({
                 </Link>
             </div>
 
-            {user ? (
-                <Link
-                    onClick={onClickClose}
-                    to={'/logout'}
-                    className="ml-auto block size-8"
-                >
-                    <BiLogOutCircle className="size-full" />
-                </Link>
-            ) : (
-                <Link
-                    onClick={onClickClose}
-                    to={'/login'}
-                    className="ml-auto block size-8"
-                >
-                    <IoMdLogIn className="size-full" />
-                </Link>
-            )}
+            <div className="flex items-center gap-3 ">
+                <div className="relative">
+                    <Link onClick={onClickClose} to={'/cart'}>
+                        <FaShoppingCart className="size-6" />
+                    </Link>
+                    <span className="absolute right-[2em] top-[1em] rounded-full bg-red-500 px-[5px] py-[0.5px] text-center text-[0.7em] text-white">
+                        {cartLenght}
+                    </span>
+                </div>
+                {user ? (
+                    <Link onClick={onClickClose} to={'/logout'}>
+                        <BiLogOutCircle className="size-6" />
+                    </Link>
+                ) : (
+                    <Link onClick={onClickClose} to={'/login'}>
+                        <FaRegUserCircle className="size-6" />
+                    </Link>
+                )}
+            </div>
         </section>
     );
 }
