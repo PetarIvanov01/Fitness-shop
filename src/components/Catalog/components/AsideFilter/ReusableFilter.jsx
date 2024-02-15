43b680a9dbdd886cc157fcc7eea0336afc966823
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-15 21:00:43 +0200

    enhance Filter component with dynamic query handling Integrated the useQuery custom hook to retrieve and update query parameters from the URL, providing better integration with routing libraries like React Router.

--- src/components/Catalog/components/AsideFilter/ReusableFilter.jsx
+++ src/components/Catalog/components/AsideFilter/ReusableFilter.jsx
@@ -1,10 +1,36 @@
-export default function Filter({ title, options }) {
+import { useEffect, useState } from 'react';
+import useQuery from '../../../../hooks/useQuery';
+
+export default function Filter({ id, title, options }) {
+    const { queryObj, handleQueryChange } = useQuery();
+    const [selectedOption, setSelectedOption] = useState(queryObj[id] || '');
+
+    useEffect(() => {
+        setSelectedOption(queryObj[id] || '');
+    }, [queryObj, id]);
+
+    const onSelectChangeQuery = (e) => {
+        const queryParam = e.target.id;
+        const queryValue = e.target.value;
+
+        handleQueryChange({
+            [queryParam]: queryValue,
+        });
+
+        setSelectedOption(queryValue);
+    };
+
     return (
         <div className="mb-4 w-full">
             <p className="mb-2 text-white">{title}</p>
-            <select className="w-full rounded bg-gray-800 p-1 text-white">
+            <select
+                id={id}
+                value={selectedOption}
+                onChange={onSelectChangeQuery}
+                className="w-full rounded bg-gray-800 p-1 text-white"
+            >
                 {options.map((option) => (
-                    <option key={option.value} value={option.value}>
+                    <option key={option.value} value={option.label}>
                         {option.value}
                     </option>
                 ))}
