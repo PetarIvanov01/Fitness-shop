936ab436121205a03a3e40ce683eca69fad8d1c1
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-15 21:09:21 +0200

    refactor category parameter to querieString for better readability

--- src/zustand/catalogStore.jsx
+++ src/zustand/catalogStore.jsx
@@ -3,9 +3,9 @@ import { getCatalog } from '../api/services/catalog';
 const catalogSlice = (set) => ({
     items: [],
     isLoading: true,
-    fetch: async (category, signal) => {
+    fetch: async (querieString, signal) => {
         try {
-            const data = await getCatalog(category, signal);
+            const data = await getCatalog(querieString, signal);
             set({ items: data.values, isLoading: false });
         } catch (error) {
             // Todo implement error handling
