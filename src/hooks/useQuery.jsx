fefe843e64e69be3f95dc4f416dbf36a99e459de
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-15 21:02:12 +0200

    Implement useQuery hook for managing URL query parameters

---
+++ src/hooks/useQuery.jsx
@@ -0,0 +1,45 @@
+import { useSearchParams } from 'react-router-dom';
+import { useCallback } from 'react';
+import { RANGES } from '../utils/priceRangeConstants';
+
+export default function useQuery() {
+    const [urlQueryParams, setQueries] = useSearchParams();
+
+    const handleQueryChange = useCallback(
+        (queryObj) => {
+            for (const key in queryObj) {
+                setQueries((prev) => {
+                    if (queryObj[key] === '' && prev.has(key)) {
+                        prev.delete(key);
+                        return prev;
+                    }
+                    prev.set(key, queryObj[key]);
+
+                    return prev;
+                });
+            }
+        },
+        [setQueries]
+    );
+
+    const clearQueries = () => {
+        setQueries((prev) => {
+            const keyArr = Array.from(prev.keys());
+
+            for (const key of keyArr) {
+                prev.delete(key);
+            }
+            prev.set('from', RANGES.from), prev.set('to', RANGES.to);
+
+            return prev;
+        });
+    };
+    const queryObj = Object.fromEntries(urlQueryParams);
+
+    return {
+        queryObj,
+        urlQueryParams,
+        handleQueryChange,
+        clearQueries,
+    };
+}
