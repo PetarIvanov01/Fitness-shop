5cda52221675e17266e021ea8e47be3523f81284
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-16 23:05:05 +0200

    refactor: Update SortBy component to handle query parameters

--- src/components/Catalog/components/SortBy.jsx
+++ src/components/Catalog/components/SortBy.jsx
@@ -1,11 +1,35 @@
+import { useState } from 'react';
+import useQuery from '../../../hooks/useQuery';
+
 export default function SortBy() {
+    const { queryObj, handleQueryChange } = useQuery();
+    const [selectedOption, setSelectedOption] = useState(() => {
+        if (queryObj['sort_by'] === 'desc') {
+            return 'desc';
+        }
+        return 'asc';
+    });
+
+    const onSelectChangeQuery = (e) => {
+        const queryParam = e.target.id;
+        const queryValue = e.target.value;
+
+        handleQueryChange({
+            [queryParam]: queryValue,
+        });
+
+        setSelectedOption(queryValue);
+    };
+
     return (
         <section className="flex flex-col">
             <section className="self-end">
                 <div className="flex items-center gap-1 rounded-md bg-white p-0.5 text-gray-800">
                     <label htmlFor="sort">Sort By: </label>
                     <select
-                        id="sort"
+                        id="sort_by"
+                        value={selectedOption}
+                        onChange={onSelectChangeQuery}
                         className="rounded bg-gray-800 p-1 text-white"
                     >
                         <option value="asc">A - Z</option>
