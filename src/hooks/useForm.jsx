d73e07cb6bcf1f6e2ebe87a18eb2bd9d61a9b876
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-17 20:36:55 +0200

    refactor: Enhance useForm hook with validation functionality

--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -1,23 +1,58 @@
 import { useState } from 'react';
 
-export default function useForm(intialState = {}, onSubmit) {
+export default function useForm(
+    intialState = {},
+    onSubmit,
+    validation = () => {}
+) {
     const [values, setValues] = useState(intialState);
+    const [error, setError] = useState({
+        requestErr: null,
+    });
 
     const handleChangeValues = (e) => {
+        // const keys = Object.keys(error);
+
+        setError((state) => ({
+            requestErr: state.requestErr,
+        }));
+
+        // if (keys.length !== 1) {
+        //     setError((state) => {
+        //         const { [e.target.name]: _, ...rest } = state;
+        //         return { ...rest, requestErr: null };
+        //     });
+        // }
+
         setValues((prev) => ({
             ...prev,
             [e.target.name]: e.target.value,
         }));
     };
 
-    const handleSubmitForm = (e) => {
+    const handleSubmitForm = async (e) => {
         e.preventDefault();
-        onSubmit(values);
+        try {
+            await onSubmit(values);
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    const handleOnBlurValidation = (e) => {
+        const key = e.target.name;
+        const errorMessage = validation(key, values[key]);
+
+        if (errorMessage) {
+            setError((state) => ({ ...state, [key]: errorMessage }));
+        }
     };
 
     return {
         values,
+        error,
         handleChangeValues,
+        handleOnBlurValidation,
         handleSubmitForm,
     };
 }
