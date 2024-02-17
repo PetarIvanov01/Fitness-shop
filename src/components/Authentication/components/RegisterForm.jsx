ba3e33ffe804a353facdc6d5a1bc27c96b3119ac
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-17 20:39:24 +0200

    refactor: Enhance RegisterForm with field validation and error handling

--- src/components/Authentication/components/RegisterForm.jsx
+++ src/components/Authentication/components/RegisterForm.jsx
@@ -6,6 +6,8 @@ import Button from './Tags/Button';
 import Heading from './Tags/Heading';
 import useForm from '../../../hooks/useForm';
 import { sendUserRegistration } from '../../../api/services/user';
+import { registerFieldValidation } from '../../../validations/registerFormValidation';
+import ErrorLabel from './Tags/ErrorLabel';
 
 const initialState = {
     firstName: '',
@@ -37,46 +39,56 @@ export default function RegsiterForm() {
         }
     };
 
-    const { values, handleChangeValues, handleSubmitForm } = useForm(
-        initialState,
-        onSubmit
-    );
+    const {
+        values,
+        error,
+        handleChangeValues,
+        handleOnBlurValidation,
+        handleSubmitForm,
+    } = useForm(initialState, onSubmit, registerFieldValidation);
 
     return (
-        <div className="flex flex-col self-center pt-10 text-white">
+        <div className="flex w-full flex-col self-start pt-10 text-white">
             <Heading text={'Create a Profile'} />
 
             <form
                 onSubmit={handleSubmitForm}
-                className="flex w-full flex-col gap-2 font-alegreya font-medium"
+                className="flex w-full flex-col gap-4 font-alegreya font-medium"
             >
-                <div className="w-full">
+                <div className=" h-16 max-w-96">
                     <Label htmlFor={'firstName'} text={'First name'} />
                     <Input
+                        onblur={handleOnBlurValidation}
                         handler={handleChangeValues}
                         value={values.firstName}
                         id={'firstName'}
                         name={'firstName'}
                         placeholder={'First name...'}
                         type={'text'}
+                        error={!!error?.firstName}
                     />
+                    <ErrorLabel error={error?.firstName} />
                 </div>
 
-                <div className="w-full">
+                <div className="h-16 max-w-96">
                     <Label htmlFor={'lastName'} text={'Last name'} />
                     <Input
+                        onblur={handleOnBlurValidation}
                         handler={handleChangeValues}
                         value={values.lastName}
                         id={'lastName'}
                         name={'lastName'}
                         placeholder={'Last name...'}
                         type={'text'}
+                        error={!!error?.lastName}
                     />
+                    <ErrorLabel error={error?.lastName} />
                 </div>
 
-                <div className="w-full">
+                <div className=" h-16 max-w-96">
                     <Label htmlFor={'phoneNumber'} text={'Phone number'} />
                     <Input
+                        onblur={handleOnBlurValidation}
                         handler={handleChangeValues}
                         value={values.phoneNumber}
                         id={'phoneNumber'}
@@ -85,12 +97,15 @@ export default function RegsiterForm() {
                         type={'tel'}
                         pattern="\+[0-9]{3} [0-9]{2} [0-9]{3} [0-9]{4}"
                         title="Please provide valid phone number"
+                        error={!!error?.phoneNumber}
                     />
+                    <ErrorLabel error={error?.phoneNumber} />
                 </div>
 
-                <div className="w-full">
+                <div className="h-16 max-w-96">
                     <Label htmlFor={'email'} text={'Email address'} />
                     <Input
+                        onblur={handleOnBlurValidation}
                         handler={handleChangeValues}
                         value={values.email}
                         id={'email'}
@@ -98,31 +113,39 @@ export default function RegsiterForm() {
                         placeholder={'example@email.com'}
                         type={'email'}
                         title="Please provide valid email address"
+                        error={!!error?.email}
                     />
+                    <ErrorLabel error={error?.email} />
                 </div>
 
-                <div className="w-full">
+                <div className="h-16 max-w-96">
                     <Label htmlFor={'password'} text={'Password'} />
                     <Input
+                        onblur={handleOnBlurValidation}
                         handler={handleChangeValues}
                         value={values.password}
                         id={'password'}
                         name={'password'}
                         placeholder={'Password...'}
                         type={'password'}
+                        error={!!error?.password}
                     />
+                    <ErrorLabel error={error?.password} />
                 </div>
 
-                <div className="w-full">
+                <div className="h-16 max-w-96">
                     <Label htmlFor={'re-password'} text={'Password'} />
                     <Input
+                        onblur={handleOnBlurValidation}
                         handler={handleChangeValues}
                         value={values['re-password']}
                         id={'re-password'}
                         name={'re-password'}
                         placeholder={'Repeat password...'}
                         type={'password'}
+                        error={!!error['re-password']}
                     />
+                    <ErrorLabel error={error['re-password']} />
                 </div>
 
                 <Button text={'Sign Up'} />
