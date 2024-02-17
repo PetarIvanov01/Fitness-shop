9ee07afa00fe5c672388f98dbe4045e1e0f10500
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-17 20:38:40 +0200

    refactor: Enhance Input component with error handling and focus/blur events

--- src/components/Authentication/components/Tags/Input.jsx
+++ src/components/Authentication/components/Tags/Input.jsx
@@ -7,12 +7,17 @@ export default function Input({
     value,
     pattern = null,
     title = 'Field is required',
+    onblur,
+    onfocus,
+    error,
 }) {
     return (
         <input
-            className="w-full border-[1px] bg-gray-800 p-1 hover:opacity-65 focus:opacity-65"
+            className={`${error && 'border-red-500'} w-full border-[1px] bg-gray-800 p-1 hover:opacity-65 focus:bg-slate-800 focus:opacity-65 focus:outline-none focus:ring-2 focus:ring-blue-300`}
             value={value}
             onChange={handler}
+            onBlur={onblur}
+            onFocus={onfocus}
             placeholder={placeholder}
             type={type}
             name={name}
