a513d9d68c552b016909661534cb95b695bdd029
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-19 21:18:41 +0200

    Updated the catalog slice to include the total number of items in the database (itemsLngInDb).

--- src/zustand/catalogStore.jsx
+++ src/zustand/catalogStore.jsx
@@ -1,12 +1,17 @@
 import { getCatalog } from '../api/services/catalog';
 
 const catalogSlice = (set) => ({
+    itemsLngInDb: 0,
     items: [],
     isLoading: true,
     fetch: async (querieString, signal) => {
         try {
             const data = await getCatalog(querieString, signal);
-            set({ items: data.values, isLoading: false });
+            set({
+                items: data.result,
+                isLoading: false,
+                itemsLngInDb: data.itemsLng,
+            });
         } catch (error) {
             // Todo implement error handling
             // console.log(error);
