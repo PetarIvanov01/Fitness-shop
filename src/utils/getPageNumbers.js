2527e068cdaa916cb12128c8488d356dbdb7768e
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-19 21:16:59 +0200

    Implemented a function to calculate the array of page numbers to display in the pagination component.

---
+++ src/utils/getPageNumbers.js
@@ -0,0 +1,20 @@
+export default function getPageNumbers(currentPage, totalPages) {
+    const MAX_BUTTONS = 3;
+    const halfNumButtons = Math.floor(MAX_BUTTONS / 2);
+
+    let startPage = currentPage - halfNumButtons;
+    let endPage = currentPage + halfNumButtons;
+
+    if (startPage < 1) {
+        startPage = 1;
+        endPage = Math.min(totalPages, MAX_BUTTONS);
+    } else if (endPage > totalPages) {
+        endPage = totalPages;
+        startPage = Math.max(1, totalPages - MAX_BUTTONS + 1);
+    }
+
+    return Array.from(
+        { length: endPage - startPage + 1 },
+        (_, index) => startPage + index
+    );
+}
