8744a377b70f00d56a7628525fa15dfc589dab9c
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-19 21:14:00 +0200

    Refactored Pagination component to support dynamic page navigation with query parameters.

--- src/components/Catalog/components/Pagination.jsx
+++ src/components/Catalog/components/Pagination.jsx
@@ -1,19 +1,45 @@
 import { GrLinkNext, GrLinkPrevious } from 'react-icons/gr';
+import useQuery from '../../../hooks/useQuery';
+import ButtonPage from './PaginationButtons';
+import useStore from '../../../zustand/store';
 
 export default function Pagination() {
+    const { queryObj, handleQueryChange } = useQuery();
+    const itemsLength = useStore((state) => state.itemsLngInDb);
+
+    const page = Number(queryObj.page) || 1;
+    const perPage = Number(queryObj.perPage) || 4;
+
+    const allPages = Math.ceil(itemsLength / perPage);
+
+    const handleNext = () => {
+        if (!isNaN(page) && page > 0 && page < allPages) {
+            handleQueryChange({
+                page: page + 1,
+                perPage,
+            });
+        }
+    };
+    const handlePrev = () => {
+        if (!isNaN(page) && page > 1 && page <= allPages) {
+            handleQueryChange({
+                page: page - 1,
+                perPage,
+            });
+        }
+    };
+
     return (
-        <div className="mt-auto flex justify-end pb-1">
-            <div className="flex gap-2">
-                <button className="size-6">
-                    <GrLinkPrevious className="size-full text-white" />
-                </button>
+        <div className="mt-auto flex justify-end gap-2 pb-1">
+            <button onClick={handlePrev} className="size-6">
+                <GrLinkPrevious className="size-full text-white" />
+            </button>
 
-                <button className="text-white ">[ 1 ][ 2 ][ 3 ]</button>
+            <ButtonPage current={page} pages={allPages} />
 
-                <button className="size-6">
-                    <GrLinkNext className="size-full text-white" />
-                </button>
-            </div>
+            <button onClick={handleNext} className="size-6">
+                <GrLinkNext className="size-full text-white" />
+            </button>
         </div>
     );
 }
