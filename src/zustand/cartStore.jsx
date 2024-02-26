d4a7831266a41415b81946d26f6b8f26cba026c1
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-26 18:08:44 +0200

    Removed the length property from the initial state and computed its value dynamically based on the contents of the cart. Renamed the addCartDataToCookies function to addCartItemIntoStore to reflect its purpose more accurately. Introduced a new function removeCartItem to remove a specific item from the cart. Modified the clearCartData function to clearCartItem and added the ability to clear a specific product from the cart or the entire cart. Renamed the addCartItemToStore function to addCartItemIntoStore for consistency. Introduced a new helper function addCartItemsToCookies to manage the addition of cart items to cookies. Updated the fetchCartData function to use the getCart service and update the cart state accordingly.

--- src/zustand/cartStore.jsx
+++ src/zustand/cartStore.jsx
@@ -5,6 +5,7 @@ const COOKIE_NAME = 'cart-cookie';
 const COOKIE_OPTIONS = { expires: 1, sameSite: 'Strict', path: '/' };
 
 const cartStore = (set) => ({
+    cart: [],
     length: (() => {
         try {
             const items = Cookies.get(COOKIE_NAME);
@@ -12,39 +13,46 @@ const cartStore = (set) => ({
                 return 0;
             }
             const parsedCookieData = JSON.parse(items);
-            return Array.isArray(parsedCookieData)
-                ? parsedCookieData.length
-                : 0;
+            return parsedCookieData.length ? parsedCookieData.length : 0;
         } catch (error) {
             console.error('Error while calculating length:', error);
             return 0;
         }
     })(),
-    cart: [],
-    addCartDataToCookies: (cartItemId) => {
-        const currentCookieData = Cookies.get(COOKIE_NAME);
+    removeCartItem: (cartItemId) => {
+        const currentCookieItems = Cookies.get(COOKIE_NAME);
+        const parsedCookie = JSON.parse(currentCookieItems);
 
-        if (currentCookieData === undefined) {
-            Cookies.set(
-                COOKIE_NAME,
-                JSON.stringify([cartItemId]),
-                COOKIE_OPTIONS
-            );
-            set({ length: 1 });
-            return;
+        const items = parsedCookie.cartItems;
+
+        parsedCookie.length--;
+        items[cartItemId]--;
+
+        if (items[cartItemId] === 0) {
+            delete items[cartItemId];
         }
 
-        const parsedCookieData = JSON.parse(currentCookieData);
-        const setOfCookieData = new Set(parsedCookieData);
-
-        if (setOfCookieData.has(cartItemId)) {
-            return;
-        }
-        setOfCookieData.add(cartItemId);
-        set({ length: setOfCookieData.size });
-
-        const stringifiedCookieData = JSON.stringify([...setOfCookieData]);
+        const stringifiedCookieData = JSON.stringify(parsedCookie);
         Cookies.set('cart-cookie', stringifiedCookieData, COOKIE_OPTIONS);
+
+        set((state) => {
+            let cart = [...state.cart];
+            const existingItem = cart.find(
+                (cartItem) => cartItem.product_id === cartItemId
+            );
+
+            existingItem.quantity--;
+
+            if (existingItem.quantity === 0) {
+                cart = cart.filter((e) => e.product_id !== cartItemId);
+            }
+
+            return {
+                ...state,
+                cart,
+                length: parsedCookie.length,
+            };
+        });
     },
     fetchCartData: async (signal) => {
         const currentCookieData = Cookies.get(COOKIE_NAME);
@@ -55,22 +63,85 @@ const cartStore = (set) => ({
 
         set({ cart: data });
     },
-    clearCartData: () => {
-        Cookies.remove(COOKIE_NAME, COOKIE_OPTIONS);
-        set({ cart: [], length: 0 });
-    },
-    setCartItemIntoStore: (item) => {
-        set(({ cart }) => {
-            const exist = cart.some((e) => e.product_id === item.product_id);
-            if (exist) return { cart };
+    clearCartItem: (productId = undefined) => {
+        if (productId === undefined) {
+            Cookies.remove(COOKIE_NAME, COOKIE_OPTIONS);
+            set({ cart: [], length: 0 });
+            return;
+        }
 
-            const newCart = [...cart, item];
+        const currentCookieItems = Cookies.get(COOKIE_NAME);
+        const parsedCookie = JSON.parse(currentCookieItems);
 
+        if (parsedCookie.cartItems[productId]) {
+            parsedCookie.length -= parsedCookie.cartItems[productId];
+            delete parsedCookie.cartItems[productId];
+        }
+
+        const stringifiedCookieData = JSON.stringify(parsedCookie);
+        Cookies.set('cart-cookie', stringifiedCookieData, COOKIE_OPTIONS);
+
+        set((state) => {
+            const { cart } = state;
+            const existingItem = cart.find(
+                (cartItem) => cartItem.product_id === productId
+            );
+
+            let length = state.length - existingItem.quantity;
+
+            const newCart = cart.filter((p) => p.product_id !== productId);
             return {
                 cart: newCart,
+                length,
+            };
+        });
+    },
+    addCartItemIntoStore: (item) => {
+        addCartItemsToCookies(item.product_id);
+
+        set((state) => {
+            const { cart } = state;
+            const existingItem = cart.find(
+                (cartItem) => cartItem.product_id === item.product_id
+            );
+            let length = state.length + 1;
+
+            if (existingItem) {
+                existingItem.quantity++;
+                return { cart: [...cart], length };
+            }
+
+            return {
+                cart: [...cart, { ...item, quantity: 1 }],
+                length,
             };
         });
     },
 });
 
 export default cartStore;
+
+function addCartItemsToCookies(cartItemId) {
+    const currentCookieData = Cookies.get(COOKIE_NAME);
+
+    if (currentCookieData === undefined) {
+        Cookies.set(
+            COOKIE_NAME,
+            JSON.stringify({ cartItems: { [cartItemId]: 1 }, length: 1 }),
+            COOKIE_OPTIONS
+        );
+        return;
+    }
+
+    const parsedCookieData = JSON.parse(currentCookieData);
+
+    if (parsedCookieData.cartItems[cartItemId]) {
+        parsedCookieData.cartItems[cartItemId]++;
+    } else {
+        parsedCookieData.cartItems[cartItemId] = 1;
+    }
+
+    parsedCookieData.length++;
+    const stringifiedCookieData = JSON.stringify(parsedCookieData);
+    Cookies.set('cart-cookie', stringifiedCookieData, COOKIE_OPTIONS);
+}
