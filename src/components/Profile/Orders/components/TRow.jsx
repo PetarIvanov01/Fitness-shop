0207dc364192297e73405d6beafbe897f9a33efe
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-27 13:24:05 +0200

    Implemented the table row component (TRow) to display order details including date, order ID, product quantity, total price, and status. Added color coding for different order statuses for better visualization.

---
+++ src/components/Profile/Orders/components/TRow.jsx
@@ -0,0 +1,35 @@
+const STATUS_COLORS = {
+    Canceled: 'text-red-500',
+    Finished: 'text-green-500',
+    'In Process': 'text-orange-500',
+};
+export default function TRow({
+    id,
+    createdAt,
+    order_id,
+    quantity,
+    priceForAllProducts,
+    status,
+}) {
+    const statusColor = STATUS_COLORS[status];
+
+    return (
+        <tr key={id} className="border-b max-sm:text-[0.7em]">
+            <td className="p-2 text-center">
+                <span>{createdAt}</span>
+            </td>
+            <td className="p-2 text-center hover:cursor-pointer hover:underline">
+                <span className="text-blue-300">{order_id}</span>
+            </td>
+            <td className="p-2 text-center">
+                <span>{quantity}</span>
+            </td>
+            <td className="p-2 text-center">
+                <span>$ {priceForAllProducts.toFixed(2)}</span>
+            </td>
+            <td className={`p-2 text-center ${statusColor}`}>
+                <span>{status}</span>
+            </td>
+        </tr>
+    );
+}
