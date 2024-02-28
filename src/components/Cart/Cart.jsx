222425a3d70d8b776bde227fb9a41c13e74ab516
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-28 22:51:27 +0200

    Refactored the Cart component to separate its structure into smaller components for better organization and readability.

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,35 +1,15 @@
-import { Link } from 'react-router-dom';
-
-import TableCart from './components/Table/CartTable';
-import CheckoutSection from './components/CheckoutSection';
-import CartHeader from './components/Table/CartHeader';
+import CartHeader from './components/CartHeader';
+import CartFooter from './components/CartFooter';
+import CartBody from './components/CartBody';
 
 export default function Cart() {
     return (
         <div className="h-auto font-alegreya">
             <CartHeader />
 
-            <section className="flex max-h-full flex-col gap-8 bg-slate-800 bg-opacity-95 text-white">
-                <TableCart />
+            <CartBody />
 
-                <CheckoutSection />
-
-                <div className="rounded-md bg-slate-800 bg-opacity-85 p-4">
-                    <h2 className="mb-4 text-2xl font-bold">
-                        Continue Shopping
-                    </h2>
-                    <p className="text-gray-300">
-                        Explore more fitness equipment and accessories.
-                        <Link
-                            to={'/catalog'}
-                            className="ml-1 underline hover:no-underline"
-                        >
-                            Go back to shopping
-                        </Link>
-                        .
-                    </p>
-                </div>
-            </section>
+            <CartFooter />
         </div>
     );
 }
