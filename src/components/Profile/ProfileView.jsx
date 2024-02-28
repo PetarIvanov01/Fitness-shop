a34e4d4f03d2accc98f4022304e4d9ff72905be7
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-28 22:54:41 +0200

    Refactored the ProfileView component to utilize the ProfileForm component for displaying user information and editing capabilities.

--- src/components/Profile/ProfileView.jsx
+++ src/components/Profile/ProfileView.jsx
@@ -1,34 +1,16 @@
-import { getUserInformation } from '../../api/services/user';
-
-import { useState } from 'react';
-import useFetch from '../../hooks/useFetch';
-import { useParams } from 'react-router-dom';
-import PersonalInfo from './components/PersonalInfo';
-import ShippingInfo from './components/ShippingInfo';
-import EditSaveToggleButton from './components/EditSaveToggleButton';
 import OrderView from './Orders/OrderView';
 import Header from './components/Header';
+import ProfileForm from './ProfileForm';
+import useStore from '../../zustand/store';
 
 export default function ProfileView() {
-    const { userId } = useParams();
-    const { data } = useFetch(getUserInformation, userId);
-
-    const [isEditing, setIsEditing] = useState(true);
+    const user = useStore((state) => state.user);
 
     return (
         <section className="my-20 flex flex-col bg-gradient-to-l from-slate-800/95 to-slate-900/95 px-10 py-8 max-sm:text-[0.8em]">
-            <Header fullName={data.fullName} />
+            <Header fullName={user.fullName} />
 
-            <div className="relative">
-                <PersonalInfo isEditing={isEditing} {...data} />
-                <div className="absolute right-[50%] my-2 h-[1px] w-2/3 translate-x-1/2 bg-white"></div>
-                <ShippingInfo isEditing={isEditing} />
-
-                <EditSaveToggleButton
-                    isEditing={isEditing}
-                    setIsEditing={setIsEditing}
-                />
-            </div>
+            <ProfileForm />
 
             <OrderView />
         </section>
