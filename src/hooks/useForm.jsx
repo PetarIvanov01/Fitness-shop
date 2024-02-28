63d7f5ba1557f89824330fb8c127cda5c01c463d
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-28 22:50:53 +0200

    Refactored the useForm hook to utilize useCallback for the event handlers handleChangeValues, handleSubmitForm, and handleOnBlurValidation to optimize performance by memoizing these functions and preventing unnecessary re-renders.

--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 export default function useForm(
     intialState = {},
@@ -11,48 +11,60 @@ export default function useForm(
         requestErr: null,
     });
 
-    const handleChangeValues = (e) => {
-        const keys = Object.keys(error);
+    const handleChangeValues = useCallback(
+        (e) => {
+            const keys = Object.keys(error);
 
-        if (keys.length !== 1) {
+            if (keys.length !== 1) {
+                setError((state) => {
+                    /*eslint no-unused-vars: "off"*/
+                    const { [e.target.name]: _, ...rest } = state;
+                    return { ...rest, requestErr: null, isVisible: false };
+                });
+            }
+
+            setValues((prev) => ({
+                ...prev,
+                [e.target.name]: e.target.value,
+            }));
+        },
+        [error]
+    );
+
+    const handleSubmitForm = useCallback(
+        async (e) => {
+            e.preventDefault();
+            try {
+                await onSubmit(values);
+            } catch (error) {
+                if (error.message) {
+                    setError((state) => ({
+                        ...state,
+                        requestErr: error.message,
+                    }));
+                }
+            }
+        },
+        [onSubmit, values]
+    );
+
+    const handleOnBlurValidation = useCallback(
+        (e) => {
+            const key = e.target.name;
+            const errorMessage = validation(key, values[key]);
+
             setError((state) => {
-                /*eslint no-unused-vars: "off"*/
-                const { [e.target.name]: _, ...rest } = state;
-                return { ...rest, requestErr: null, isVisible: false };
+                const obj = { ...state };
+
+                if (errorMessage) {
+                    obj[key] = errorMessage;
+                }
+
+                return { ...obj, isVisible: true };
             });
-        }
-
-        setValues((prev) => ({
-            ...prev,
-            [e.target.name]: e.target.value,
-        }));
-    };
-
-    const handleSubmitForm = async (e) => {
-        e.preventDefault();
-        try {
-            await onSubmit(values);
-        } catch (error) {
-            if (error.message) {
-                setError((state) => ({ ...state, requestErr: error.message }));
-            }
-        }
-    };
-
-    const handleOnBlurValidation = (e) => {
-        const key = e.target.name;
-        const errorMessage = validation(key, values[key]);
-
-        setError((state) => {
-            const obj = { ...state };
-
-            if (errorMessage) {
-                obj[key] = errorMessage;
-            }
-
-            return { ...obj, isVisible: true };
-        });
-    };
+        },
+        [validation, values]
+    );
 
     return {
         values,
