e4063904b3310f41c913fe371e1cf0e5a88c922a
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-28 23:00:23 +0200

    update: Input component to handle dynamic input changes
    - Modify Input component to accept name and handleOnChange props to handle dynamic input changes

--- src/components/Profile/components/Input.jsx
+++ src/components/Profile/components/Input.jsx
@@ -1,12 +1,15 @@
-export default function Input({ value, isEdit = false }) {
-    const editStyles = `${isEdit ? ' ' : 'hover:opacity-75 bg-white '}`;
+export default function Input({ name, value, isEdit = false, handleOnChange }) {
+    const editStyles = `${isEdit ? 'text-gray-300 ' : 'hover:opacity-75 text-white '}`;
 
     return (
         <input
             disabled={isEdit}
             type="text"
-            className={`max-w-[250px] border-[1px] bg-zinc-700 p-1 text-white ${editStyles} focus:bg-slate-800 focus:opacity-95 focus:outline-none focus:ring-1 focus:ring-blue-300`}
-            defaultValue={value}
+            name={name}
+            onChange={handleOnChange}
+            className={`max-w-[250px] border-[1px] bg-zinc-700 p-1  ${editStyles}
+         focus:bg-slate-800 focus:opacity-95 focus:outline-none focus:ring-1 focus:ring-blue-300`}
+            value={value}
         />
     );
 }
