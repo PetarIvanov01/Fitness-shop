997d1e0e967b12aac0f811ae8b0cebbe636f7206
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-03-02 21:57:52 +0200

    update styles

--- src/components/Main/TopOffer/components/ControlOfferButton.jsx
+++ src/components/Main/TopOffer/components/ControlOfferButton.jsx
@@ -1,7 +1,7 @@
 export default function ControlOfferButton({ icon, alt, handler }) {
     return (
         <div
-            className="mx-2 flex min-h-12 min-w-8 items-center justify-center rounded-full border hover:scale-105
+            className="mx-2 flex h-max min-h-12 min-w-8 items-center justify-center rounded-full border hover:scale-105
          hover:opacity-40 max-sm:size-12"
         >
             <img
