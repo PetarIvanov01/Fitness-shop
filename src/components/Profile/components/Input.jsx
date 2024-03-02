920cb907bb83c1ae65d10ad3ead6cecd272892b8
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-03-02 21:57:12 +0200

    update: Improved Input component
    - Added support for custom width and placeholder in the Input component.
    - The width can be specified as either a number (interpreted as pixels) or a string (e.g., 'full', '1/2', '1/3', etc.).

--- src/components/Profile/components/Input.jsx
+++ src/components/Profile/components/Input.jsx
@@ -1,13 +1,22 @@
-export default function Input({ name, value, isEdit = false, handleOnChange }) {
+export default function Input({
+    name,
+    value,
+    isEdit = false,
+    handleOnChange,
+    width = 200,
+    placeholder,
+}) {
     const editStyles = `${isEdit ? 'text-gray-300 ' : 'hover:opacity-75 text-white '}`;
-
+    const customWidth =
+        typeof width === 'number' ? `w-[${width}px]` : `w-${width}`;
     return (
         <input
+            placeholder={placeholder}
             disabled={isEdit}
             type="text"
             name={name}
             onChange={handleOnChange}
-            className={`max-w-[250px] border-[1px] bg-zinc-700 p-1  ${editStyles}
+            className={`${customWidth} border-[1px] bg-zinc-700 p-1  ${editStyles}
          focus:bg-slate-800 focus:opacity-95 focus:outline-none focus:ring-1 focus:ring-blue-300`}
             value={value}
         />
