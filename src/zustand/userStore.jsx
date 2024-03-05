a00f48cab898ff38a5a80335c8ca50e191404e4e
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-03-05 21:54:48 +0200

    update: Restructure userSlice to include personalInfo, shippingInfo, and handlers
    This commit refactors the userSlice to include additional state variables and handlers related to user information.

--- src/zustand/userStore.jsx
+++ src/zustand/userStore.jsx
@@ -1,7 +1,51 @@
 import { getFromBrowserStorage } from '../api/services/storage';
+import { getUserInformation } from '../api/services/user';
 
-const userSlice = () => ({
+const userSlice = (set) => ({
     user: getFromBrowserStorage('user'),
+    personalInfo: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phoneNumber: '',
+    },
+    shippingInfo: {
+        country: '',
+        city: '',
+        address: '',
+        postcode: '',
+    },
+    handleOnChangePersonalInfo: handleOnChangePersonalInfo(set),
+    handleOnChangeShippingInfo: handleOnChangeShippingInfo(set),
+    fetchProfile: async (userId, signal) => {
+        //Todo try to implement cache
+        const { address, ...rest } = await getUserInformation(userId, signal);
+
+        set({
+            shippingInfo: { ...address },
+            personalInfo: { ...rest },
+        });
+    },
 });
 
 export default userSlice;
+
+const handleOnChangePersonalInfo = (set) => (e) => {
+    e.preventDefault();
+    set((state) => ({
+        personalInfo: {
+            ...state.personalInfo,
+            [e.target.name]: e.target.value,
+        },
+    }));
+};
+
+const handleOnChangeShippingInfo = (set) => (e) => {
+    e.preventDefault();
+    set((state) => ({
+        shippingInfo: {
+            ...state.shippingInfo,
+            [e.target.name]: e.target.value,
+        },
+    }));
+};
