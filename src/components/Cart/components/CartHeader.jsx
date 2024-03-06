643d020e1a82bae8219d42372460665c4a35c113
PetarIvanov02 <petkata_ivanov@mail.bg>
2024-03-06 21:38:15 +0200

    update: Improve personalization in CartHeader component
    1. Refactoring user access: Updated the component to access the user's first name directly from the `personalInfo` state in the store.

--- src/components/Cart/components/CartHeader.jsx
+++ src/components/Cart/components/CartHeader.jsx
@@ -2,13 +2,14 @@ import { Link } from 'react-router-dom';
 import useStore from '../../../zustand/store';
 
 export default function CartHeader() {
-    const user = useStore((state) => state.user);
+    const firstName = useStore((state) => state.personalInfo?.firstName);
 
     return (
         <header className="flex w-full flex-col items-center gap-2 bg-slate-900 p-4 text-center opacity-90">
-            {user?.id ? (
+            {firstName ? (
                 <p className="text-green-500 ">
-                    Welcome back, Petar! Ready to make your purchase?
+                    Welcome back, <span className="font-bold">{firstName}</span>
+                    ! Ready to make your purchase?
                 </p>
             ) : (
                 <p className="text-orange-300">
