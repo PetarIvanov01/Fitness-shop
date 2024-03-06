6177bc38439ca142262f677a5521d2044ea20c79
PetarIvanov02 <petkata_ivanov@mail.bg>
2024-03-06 21:41:25 +0200

    **Update BillingSection component:**
    1. Integrated handleOnChangePersonalInfo and handleOnChangeShippingInfo for input handling

--- src/components/Checkout/components/BillingForm/BillingSection.jsx
+++ src/components/Checkout/components/BillingForm/BillingSection.jsx
@@ -7,6 +7,13 @@ import useStore from '../../../../zustand/store';
 export default function BillingSection() {
     const personalInfo = useStore((state) => state.personalInfo);
     const shippingInfo = useStore((state) => state.shippingInfo);
+
+    const handleOnChangePersonalInfo = useStore(
+        (state) => state.handleOnChangePersonalInfo
+    );
+    const handleOnChangeShippingInfo = useStore(
+        (state) => state.handleOnChangeShippingInfo
+    );
     return (
         <section className="border-b border-white px-4 pb-14 pt-12 text-white">
             <BillingHeader />
@@ -20,6 +27,7 @@ export default function BillingSection() {
                                 <LuAsterisk color="red" />
                             </Paragraph>
                             <Input
+                                handleOnChange={handleOnChangePersonalInfo}
                                 name={'firstName'}
                                 value={personalInfo.firstName}
                             />
@@ -29,6 +37,7 @@ export default function BillingSection() {
                                 Last Name <LuAsterisk color="red" />
                             </Paragraph>
                             <Input
+                                handleOnChange={handleOnChangePersonalInfo}
                                 name={'lastName'}
                                 value={personalInfo.lastName}
                             />
@@ -40,38 +49,53 @@ export default function BillingSection() {
                             Phone Number <LuAsterisk color="red" />
                         </Paragraph>
                         <Input
+                            handleOnChange={handleOnChangePersonalInfo}
                             name={'phoneNumber'}
                             value={personalInfo.phoneNumber}
                         />
                     </div>
+
                     <div>
                         <Paragraph>
                             City / Town <LuAsterisk color="red" />
                         </Paragraph>
-                        <Input name={'city'} value={shippingInfo.city} />
+                        <Input
+                            handleOnChange={handleOnChangeShippingInfo}
+                            name={'city'}
+                            value={shippingInfo.city}
+                        />
                     </div>
+
                     <div>
                         <Paragraph>
                             Street address <LuAsterisk color="red" />
                         </Paragraph>
                         <Input
+                            handleOnChange={handleOnChangeShippingInfo}
                             value={shippingInfo.address}
                             name={'address'}
                             placeholder={'House number, street name, apartment'}
                             width={'full'}
                         />
                     </div>
+
                     <div>
                         <Paragraph>
                             Country <LuAsterisk color="red" />
                         </Paragraph>
-                        <Input value={shippingInfo.country} name={'country'} />
+                        <Input
+                            handleOnChange={handleOnChangeShippingInfo}
+                            value={shippingInfo.country}
+                            name={'country'}
+                        />
                     </div>
+
                     <div>
                         <Paragraph>
                             Postcode / ZIP <LuAsterisk color="red" />
                         </Paragraph>
                         <Input
+                            handleOnChange={handleOnChangeShippingInfo}
                             value={shippingInfo.postcode}
                             name={'postcode'}
                         />
@@ -83,8 +107,13 @@ export default function BillingSection() {
                         <Paragraph>
                             Email address <LuAsterisk color="red" />
                         </Paragraph>
-                        <Input name={'email'} value={personalInfo.email} />
+                        <Input
+                            handleOnChange={handleOnChangePersonalInfo}
+                            name={'email'}
+                            value={personalInfo.email}
+                        />
                     </div>
+
                     <div>
                         <Paragraph>Extra Information (optionla)</Paragraph>
                         <textarea className="h-[100px] w-full resize-none border-2 bg-zinc-700 p-1 focus:bg-slate-800 focus:opacity-95 focus:outline-none focus:ring-1 focus:ring-blue-300" />
