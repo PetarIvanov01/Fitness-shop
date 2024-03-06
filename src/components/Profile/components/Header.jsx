05e45b57936aa207cd3462a0dfc38261a2570caf
PetarIvanov01 <123456789petar45@gmail.com>
2024-03-06 21:55:16 +0200

    Refactor Header component to use state from store

--- src/components/Profile/components/Header.jsx
+++ src/components/Profile/components/Header.jsx
@@ -1,4 +1,9 @@
-export default function Header({ fullName }) {
+import useStore from '../../../zustand/store';
+
+export default function Header() {
+    const user = useStore((state) => state.personalInfo);
+    const fullName = user.firstName.concat(' ', user.lastName);
+
     return (
         <div className="border-b-2 pb-4 text-white">
             <h1 className="text-3xl font-semibold tracking-wide">Profile</h1>
