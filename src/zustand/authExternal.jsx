56fb07a5b821746125ea8b5c7121de96b64347a4
PetarIvanov02 <petkata_ivanov@mail.bg>
2024-03-06 21:43:03 +0200

    Added clearUserInStore function to reset user-related state in the store.

--- src/zustand/authExternal.jsx
+++ src/zustand/authExternal.jsx
@@ -2,3 +2,20 @@ import useStore from './store';
 
 export const setUserInStore = (userData) =>
     useStore.setState({ user: userData });
+
+export const clearUserInStore = () =>
+    useStore.setState({
+        user: {},
+        personalInfo: {
+            firstName: '',
+            lastName: '',
+            email: '',
+            phoneNumber: '',
+        },
+        shippingInfo: {
+            country: '',
+            city: '',
+            address: '',
+            postcode: '',
+        },
+    });
