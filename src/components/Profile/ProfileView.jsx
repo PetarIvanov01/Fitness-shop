e7767b832ccf54d758b35530a9702c2c7d271d3e
PetarIvanov01 <123456789petar45@gmail.com>
2024-03-06 21:50:16 +0200

    refactor: remove user

--- src/components/Profile/ProfileView.jsx
+++ src/components/Profile/ProfileView.jsx
@@ -1,14 +1,11 @@
 import OrderView from './Orders/OrderView';
 import Header from './components/Header';
 import ProfileForm from './ProfileForm';
-import useStore from '../../zustand/store';
 
 export default function ProfileView() {
-    const user = useStore((state) => state.user);
-
     return (
         <section className="my-20 flex flex-col bg-gradient-to-l from-slate-800/95 to-slate-900/95 px-10 py-8 max-sm:text-[0.8em]">
-            <Header fullName={user.fullName} />
+            <Header />
 
             <ProfileForm />
 
