b19918308176fa4cb078d0ec7567fb8d26a44b0e
PetarIvanov01 <123456789petar45@gmail.com>
2024-03-07 20:13:34 +0200

    Create constants file

---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+export const initialProfileValue = {
+    shippingInfo: {
+        country: '',
+        city: '',
+        address: '',
+        postcode: '',
+    },
+    personalInfo: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phoneNumber: '',
+    },
+};
