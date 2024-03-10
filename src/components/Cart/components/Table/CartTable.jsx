598663d31f96c563112276963c3171cecfc269b4
PetarIvanov01 <123456789petar45@gmail.com>
2024-03-10 22:54:47 +0200

    Refactor TableCart Component:
    Created a cartItemsLengthInStore variable using useMemo to calculate the total number of items in the cart by summing up the quantities of each item.

--- src/components/Cart/components/Table/CartTable.jsx
+++ src/components/Cart/components/Table/CartTable.jsx
@@ -1,24 +1,29 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import useStore from '../../../../zustand/store';
 import TableHead from './TableHead';
 import TableRow from './TableRow';
 
 export default function TableCart() {
     const cartItems = useStore((state) => state.cart);
-    const cartLength = useStore((state) => state.length);
+    const cartLengthInCookie = useStore((state) => state.length);
     const fetchCartData = useStore((state) => state.fetchCartData);
+
+    const cartItemsLengthInStore = useMemo(() => {
+        if (cartItems.length === 0) return 0;
+        return cartItems.reduce((prev, curr) => prev + curr.quantity, 0);
+    }, [cartItems]);
 
     useEffect(() => {
         const abortController = new AbortController();
 
-        if (cartItems.length !== cartLength) {
+        if (cartItemsLengthInStore !== cartLengthInCookie) {
             fetchCartData(abortController.signal);
         }
 
         return () => {
             abortController.abort();
         };
-    }, [fetchCartData, cartItems.length, cartLength]);
+    }, [fetchCartData, cartItemsLengthInStore, cartLengthInCookie]);
 
     return (
         <div className="px-4">
