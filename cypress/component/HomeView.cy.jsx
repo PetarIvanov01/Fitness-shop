892680304a8dd0fd49cb6261376b83f2085f30fe
PetarIvanov01 <123456789petar45@gmail.com>
2024-03-12 00:27:09 +0200

    Add Cypress unit tests for home view components

---
+++ cypress/component/HomeView.cy.jsx
@@ -0,0 +1,56 @@
+/// <reference types="Cypress" />
+import '../../src/index.css';
+import { MemoryRouter } from 'react-router-dom';
+import TopOffers from '../../src/components/Main/TopOffer/TopOffer';
+import EquipCategories from '../../src/components/Main/EquipCategories/EquipCat';
+import OtherItems from '../../src/components/Main/OtherItems/OtherItems';
+
+describe('Render Home View components', () => {
+    const Component = ({ children }) => (
+        <MemoryRouter>
+            <section>{children}</section>
+        </MemoryRouter>
+    );
+
+    it('Render Top Offers section', () => {
+        cy.mount(
+            <Component>
+                <TopOffers />
+            </Component>
+        );
+        cy.get('h1').contains('Top Offers');
+        cy.get('button').contains('Order');
+    });
+
+    it('Render EquipCategories section', () => {
+        cy.mount(
+            <Component>
+                <EquipCategories />
+            </Component>
+        );
+        cy.contains('Explore Fitness');
+
+        cy.get('article').children().as('categ');
+
+        cy.get('@categ').should('have.length', 3);
+        cy.get('@categ').get('a').should('have.attr', 'href');
+
+        cy.get('@categ')
+            .first()
+            .should('have.text', 'Machines')
+            .next()
+            .should('have.text', 'Cardio')
+            .next()
+            .should('have.text', 'Free Weights');
+    });
+
+    it('Render OtherItems section', () => {
+        cy.mount(
+            <Component>
+                <OtherItems />
+            </Component>
+        );
+        cy.get('header > h1').should('have.text', 'Explore More');
+        cy.get('.animate-slide-up').contains('Price');
+    });
+});
