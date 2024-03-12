50dad360163ee590e8a82c2cde42e0206894e97f
PetarIvanov01 <123456789petar45@gmail.com>
2024-03-12 23:54:44 +0200

    Create register end-to-end tests for register view -
    Render Register View: Ensures the registration page displays properly with essential elements present.
    
    Successful Registration: Validates that a successful registration redirects the user to the home view and stores user information correctly.
    
    Prevent Registration on Input Errors: Confirms that the registration form disables submission and displays error messages for incomplete or invalid input.
    
    Registration Failure on Password Mismatch: Checks if registration fails and indicates password mismatch error when passwords don't match.

---
+++ cypress/e2e/registerView.cy.js
@@ -0,0 +1,83 @@
+/// <reference types="Cypress" />
+describe('Testing Register View', () => {
+    beforeEach(() => {
+        cy.visit('/register');
+    });
+    it('Should render the register view properly', () => {
+        cy.contains(/join the market/i);
+        cy.contains(/create a profile/i);
+    });
+    it.only('Verify that a successful registration request is intercepted, and the user is navigated to the home view.', () => {
+        let firstName = 'Test';
+        let lastName = 'Testov';
+        cy.intercept(
+            'POST',
+            'http://localhost:5000/api/v1/user/sign-up',
+            (req) => {
+                req.reply({
+                    statusCode: 200,
+                    body: {
+                        payload: {
+                            email: 'test@mail.com',
+                            fullName: `${firstName} ${lastName}`,
+                            id: '21',
+                            token: 'jwttoken',
+                        },
+                    },
+                });
+            }
+        ).as('signup');
+
+        cy.get('#firstName').type(firstName, { delay: 10 });
+        cy.get('#lastName').type(lastName, { delay: 10 });
+        cy.get('#phoneNumber').type('+359 12 342 5678', { delay: 10 });
+        cy.get('#email').type('test@mail.com', { delay: 10 });
+        cy.get('#password').type('test123456', { delay: 10 });
+        cy.get('#re-password').type('test123456', { delay: 10 });
+
+        cy.get('button[type=submit]').click();
+
+        cy.wait('@signup');
+
+        cy.url().should('eq', 'http://localhost:5173/');
+        cy.contains(/top offer/i).should('exist');
+
+        cy.getAllLocalStorage().then((result) => {
+            expect(result).to.deep.equal({
+                'http://localhost:5173': {
+                    user: '{"id":"21","token":"jwttoken"}',
+                },
+            });
+        });
+    });
+    it('Test if the registration request is prevented when there are input errors.', () => {
+        cy.get('#firstName').type('T', { delay: 10 });
+        cy.get('#lastName').type('T', { delay: 10 });
+        cy.get('#phoneNumber').type('+359 12 342 5678', { delay: 10 });
+        cy.get('#email').type('test@mail.com', { delay: 10 });
+        cy.get('#password').type('test123456', { delay: 10 });
+        cy.get('#re-password').type('test123456', { delay: 10 });
+
+        cy.get('button[type=submit]')
+            .should('be.disabled')
+            .click({ force: true });
+
+        cy.get("[data-test='error-firstName']").should('exist');
+        cy.get("[data-test='error-lastName']").should('exist');
+    });
+    it('Test the scenario where registration fails due to mismatched passwords.', () => {
+        cy.get('#firstName').type('Test', { delay: 10 });
+        cy.get('#lastName').type('Testov', { delay: 10 });
+        cy.get('#phoneNumber').type('+359 12 342 5678', { delay: 10 });
+        cy.get('#email').type('test@mail.com', { delay: 10 });
+        cy.get('#password').type('test12345', { delay: 10 });
+        cy.get('#re-password').type('test123456', { delay: 10 });
+
+        cy.get('button[type=submit]').should('not.be.disabled').click();
+
+        cy.get('#password').should('have.class', 'border-red-500');
+        cy.get('#re-password').should('have.class', 'border-red-500');
+
+        cy.contains(/Password doesn't matched/i).should('exist');
+    });
+});
