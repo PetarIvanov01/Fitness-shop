e7e458bcfa5d5d3fd82315cc388f20d583429e0c
PetarIvanov01 <123456789petar45@gmail.com>
2024-03-14 21:40:55 +0200

    Update Cypress configuration: Change port to 5172, set baseUrl for end-to-end tests, and adjust viewport dimensions.

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,22 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  port: 5173,
-
-  component: {
-    devServer: {
-      framework: "react",
-      bundler: "vite",
+    port: 5172,
+    component: {
+        devServer: {
+            framework: 'react',
+            bundler: 'vite',
+        },
+        viewportHeight: 1100,
+        viewportWidth: 910,
     },
-    viewportHeight: 1100,
-    viewportWidth: 910,
-  },
 
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        baseUrl: 'http://localhost:5173/',
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
     },
-  },
+    viewportHeight: 1150,
+    viewportWidth: 1100,
 });
