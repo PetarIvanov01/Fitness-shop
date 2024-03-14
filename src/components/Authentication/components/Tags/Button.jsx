0dbab82ae037baa9806d14fca35a172e62ed0b04
PetarIvanov01 <123456789petar45@gmail.com>
2024-03-14 22:11:55 +0200

    Enhance Button component: Add dynamic disabling based on error count.

--- src/components/Authentication/components/Tags/Button.jsx
+++ src/components/Authentication/components/Tags/Button.jsx
@@ -1,8 +1,10 @@
 import { GrLinkNext } from 'react-icons/gr';
 
-export default function Button({ text }) {
+export default function Button({ text, errors }) {
+    const isDisabled = Object.keys(errors).length > 2;
     return (
         <button
+            disabled={isDisabled}
             className="mt-2 flex w-fit items-center gap-2 rounded-sm 
                  bg-white px-1 py-1 text-black opacity-65 hover:scale-105 hover:bg-blue-400"
             type="submit"
--- src/components/Authentication/components/Tags/ErrorLabel.jsx
+++ src/components/Authentication/components/Tags/ErrorLabel.jsx
@@ -1,5 +1,12 @@
-export default function ErrorLabel({ error }) {
+export default function ErrorLabel({ error, id = '' }) {
     if (!error) return null;
 
-    return <div className=" self-end text-[0.8em] text-red-500">{error}</div>;
+    return (
+        <div
+            data-test={`error-${id}`}
+            className="self-end text-[0.8em] text-red-500"
+        >
+            {error}
+        </div>
+    );
 }
