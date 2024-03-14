57c1a53714d491562a0473f6bebe4c8ca0b2484b
PetarIvanov01 <123456789petar45@gmail.com>
2024-03-14 21:41:34 +0200

    Update ESLint configuration: Integrate Cypress plugin, add Cypress-specific rules, and declare Cypress globals.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,11 @@
 module.exports = {
     root: true,
-    env: { browser: true, es2020: true },
+    globals: {
+        cy: true,
+    },
+    env: { browser: true, es2020: true, 'cypress/globals': true },
     extends: [
+        'plugin:cypress/recommended',
         'eslint:recommended',
         'plugin:react/recommended',
         'plugin:react/jsx-runtime',
@@ -10,12 +14,18 @@ module.exports = {
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
     settings: { react: { version: '18.2' } },
-    plugins: ['react-refresh'],
+    plugins: ['react-refresh', 'cypress'],
     rules: {
         'react/prop-types': 'off',
         'react-refresh/only-export-components': [
             'warn',
             { allowConstantExport: true },
         ],
+        'cypress/no-assigning-return-values': 'error',
+        'cypress/no-unnecessary-waiting': 'error',
+        'cypress/assertion-before-screenshot': 'warn',
+        'cypress/no-force': 'warn',
+        'cypress/no-async-tests': 'error',
+        'cypress/no-pause': 'error',
     },
 };
