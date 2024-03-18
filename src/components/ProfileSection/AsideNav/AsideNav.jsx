b0740e0b4c78f519b47ec881b3703468b733e3d4
PetarIvanov01 <123456789petar45@gmail.com>
2024-03-18 23:16:12 +0200

    update: added userId to all of the links

--- src/components/ProfileSection/AsideNav/AsideNav.jsx
+++ src/components/ProfileSection/AsideNav/AsideNav.jsx
@@ -7,7 +7,7 @@ import { IoMdInformationCircleOutline } from 'react-icons/io';
 import ProfilePic from './components/ProfilePic';
 import { NavLi } from './components/NavLi';
 
-export default function AsideNav() {
+export default function AsideNav({ userId }) {
     return (
         <div className="flex w-1/3 min-w-[220px] max-w-[250px] flex-col gap-2 bg-[#1B263E] bg-opacity-65">
             <ProfilePic />
@@ -15,19 +15,23 @@ export default function AsideNav() {
             <nav className="h-full rounded-b-md border-2 border-black bg-[#1B263E] bg-opacity-65 text-white">
                 <ul className="flex flex-col justify-center gap-3 px-2 pt-4">
                     <NavLi Icon={IoMdInformationCircleOutline}>
-                        <Link to={'/profile/info'}>Personal Information</Link>
+                        <Link to={`/profile/info/${userId}`}>
+                            Personal Information
+                        </Link>
                     </NavLi>
 
                     <NavLi Icon={LuMapPin}>
-                        <Link to={'/profile/address'}>Addreses</Link>
+                        <Link to={`/profile/address/${userId}`}>Addreses</Link>
                     </NavLi>
 
                     <NavLi Icon={IoTicketOutline}>
-                        <Link to={'/profile/orders'}>My Orders</Link>
+                        <Link to={`/profile/orders/${userId}`}>My Orders</Link>
                     </NavLi>
 
                     <NavLi Icon={MdOutlinePayment}>
-                        <Link to={'/profile/pers-info'}>Payment Methods</Link>
+                        <Link to={`/profile/pay-methods/${userId}`}>
+                            Payment Methods
+                        </Link>
                     </NavLi>
                 </ul>
             </nav>
