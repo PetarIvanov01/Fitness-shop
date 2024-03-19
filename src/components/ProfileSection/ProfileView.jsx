b89c80870e6ddcda64a052768eed7f176be496f3
PetarIvanov01 <123456789petar45@gmail.com>
2024-03-19 18:35:28 +0200

    Refactor ProfileView component to dynamically render a section based on outlet context
    - Updated the ProfileView component to use the useOutletContext hook from react-router-dom to access the context provided by the outlet.
    - Extracted the userId and RenderedSection from the outlet context.
    - Modified the component to render the AsideNav component with the userId passed as a prop.
    - Dynamically rendered the section using the RenderedSection component obtained from the outlet context, passing the userId as a prop.

--- src/components/ProfileSection/ProfileView.jsx
+++ src/components/ProfileSection/ProfileView.jsx
@@ -1,14 +1,19 @@
+import { useOutletContext } from 'react-router-dom';
 import AsideNav from './AsideNav/AsideNav';
-import PersonalInfoSection from './PersonalInfo/PersonalInfoSection';
 
 export default function ProfileView() {
+    const context = useOutletContext();
+    const userId = context.userId;
+
+    const RenderedSection = context.RenderedSection;
+
     return (
         <section className="flex min-h-[800px] w-full justify-center bg-slate-900 bg-opacity-95 font-inter">
             <div className="flex w-full justify-center gap-4 p-2 max-ml:flex-col max-ml:items-center max-ml:justify-normal">
-                <AsideNav />
+                <AsideNav userId={userId} />
 
                 <section className="w-full max-w-[560px] rounded-md border-2 border-black bg-[#1B263E] bg-opacity-65">
-                    <PersonalInfoSection />
+                    <RenderedSection userId={context.userId} />
                 </section>
             </div>
         </section>
