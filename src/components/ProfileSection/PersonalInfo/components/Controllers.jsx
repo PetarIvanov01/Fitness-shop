a2243910109c312ea5fe186751ae5bd81f18aa46
PetarIvanov01 <123456789petar45@gmail.com>
2024-03-20 22:24:23 +0200

    Refactored Controllers component to include a confirmation modal for saving changes.

--- src/components/ProfileSection/PersonalInfo/components/Controllers.jsx
+++ src/components/ProfileSection/PersonalInfo/components/Controllers.jsx
@@ -1,12 +1,44 @@
-export default function Controllers() {
+import { useState } from 'react';
+import ConfirmModal from '../../components/ConfirmModal';
+
+export default function Controllers({ handleOnSubmit, hasChange }) {
+    const [showModal, setShowModal] = useState(false);
+
+    const toggleModal = (e) => {
+        e.preventDefault();
+        if (hasChange) {
+            setShowModal(!showModal);
+        }
+    };
+
+    const onAcceptCloseModal = (e) => {
+        e.preventDefault();
+        handleOnSubmit();
+        setShowModal(false);
+    };
+
+    const btnStyles = `${hasChange ? 'bg-blue-600 text-white' : 'bg-gray-500 text-red-50'}`;
+
     return (
         <div className="flex gap-6 self-end pt-10">
-            <button className="rounded-lg bg-blue-600 px-5 py-1 hover:opacity-50">
-                Save
+            <button
+                type="submit"
+                onClick={toggleModal}
+                className={`${btnStyles} rounded-lg px-5 py-1 text-center
+                      transition-all hover:opacity-50 hover:shadow-lg`}
+            >
+                Save Changes
             </button>
-            <button className="rounded-lg bg-gray-400 px-5 py-1 hover:opacity-50">
-                Cancel
-            </button>
+
+            {showModal && (
+                <ConfirmModal
+                    text={
+                        'Are you sure you want to edit your personal information?'
+                    }
+                    toggleModal={toggleModal}
+                    onAcceptCloseModal={onAcceptCloseModal}
+                />
+            )}
         </div>
     );
 }
