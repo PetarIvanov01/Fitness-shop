f9aea0697fe77e087bf432b64d8649a5b3f5b721
PetarIvanov01 <123456789petar45@gmail.com>
2024-03-21 13:36:38 +0200

    feat: Added AddressBody component
    - Implemented the AddressBody component responsible for rendering address-related information and functionalities within the user profile.
    - Incorporated the useProfileCache hook to manage caching of address data and facilitate efficient data fetching.
    - Integrated FormSection component to display and manage address information.
    - Included OtherAddresses component to handle additional address-related functionalities.
    - Implemented conditional rendering based on isLoading state to display either address data or a skeleton loader while data is being fetched.

---
+++ src/components/ProfileSection/AddressInfo/AddressBody.jsx
@@ -0,0 +1,33 @@
+import useStore from '../../../zustand/store';
+import useProfileCache from '../../../hooks/useProfileCache';
+import FormSection from './components/FormSection';
+import Skeleton from '../PersonalInfo/components/Skeleton';
+import OtherAddresses from './OtherAddresses';
+
+export default function AddressBody({ userId }) {
+    const fetchAddress = useStore((state) => state.fetchAddress);
+    const shippingInfo = useStore((state) => state.shippingInfo);
+
+    const { data, isLoading, emptyValue } = useProfileCache(
+        userId,
+        shippingInfo,
+        fetchAddress
+    );
+
+    return (
+        <>
+            {isLoading ? (
+                <>
+                    <FormSection
+                        emptyValue={emptyValue}
+                        shippingInfo={data}
+                        userId={userId}
+                    />
+                    <OtherAddresses />
+                </>
+            ) : (
+                <Skeleton />
+            )}
+        </>
+    );
+}
