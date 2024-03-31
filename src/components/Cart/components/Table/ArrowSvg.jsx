988521c374549560bedeebd500c04ea6e68f43ff
PetarIvanov01 <123456789petar45@gmail.com>
2024-03-31 14:15:19 +0300

    updated the TableCart component to render a message - Improved the user experience by providing clear feedback when the cart is empty and guiding users to explore products.

---
+++ src/components/Cart/components/Table/ArrowSvg.jsx
@@ -0,0 +1,44 @@
+export default function ArrowSvg() {
+    return (
+        <svg
+            className="absolute w-96 -rotate-3 opacity-55 max-[670px]:invisible"
+            version="1.1"
+            viewBox="0 0 800 800"
+        >
+            <g
+                strokeWidth="10"
+                stroke="#5479be"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                transform="matrix(0.9998476951563913,0.01745240643728351,-0.01745240643728351,0.9998476951563913,11.041884512356887,-6.920040637469924)"
+            >
+                <path
+                    d="M216.4660623073578 214.91708374023438Q180.4660623073578 592.9170837402344 586.4660623073578 584.9170837402344 "
+                    markerEnd="url(#SvgjsMarker1429)"
+                ></path>
+            </g>
+            <defs>
+                <marker
+                    markerWidth="12"
+                    markerHeight="12"
+                    refX="6"
+                    refY="6"
+                    viewBox="0 0 12 12"
+                    orient="auto"
+                    id="SvgjsMarker1429"
+                >
+                    <polyline
+                        points="0,6 6,3 0,0"
+                        fill="none"
+                        strokeWidth="2"
+                        stroke="#5479be"
+                        strokeLinecap="round"
+                        transform="matrix(1,0,0,1,2,3)"
+                        strokeLinejoin="round"
+                    ></polyline>
+                </marker>
+            </defs>
+        </svg>
+    );
+}
--- src/components/Cart/components/Table/CartTable.jsx
+++ src/components/Cart/components/Table/CartTable.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import useStore from '../../../../zustand/store';
 import TableHead from './TableHead';
 import TableRow from './TableRow';
+import ArrowSvg from './ArrowSvg';
 
 export default function TableCart() {
     const cartItems = useStore((state) => state.cart);
@@ -27,15 +28,42 @@ export default function TableCart() {
 
     return (
         <div className="px-4">
-            <table className="w-full">
-                <TableHead />
+            {cartItemsLengthInStore > 0 ? (
+                <table className="w-full">
+                    <TableHead />
+                    <tbody>
+                        {cartItems.map((e) => (
+                            <TableRow key={e.product_id} {...e} />
+                        ))}
+                    </tbody>
+                </table>
+            ) : (
+                <div className="relative flex w-fit flex-col pl-9 pt-9 max-[670px]:gap-4 max-[670px]:text-center">
+                    <div className="flex flex-col gap-2 text-center text-2xl text-orange-500">
+                        <p className="font-semibold">
+                            Oops! Your cart is empty.
+                        </p>
+                        <p className="text-neutral-400">
+                            It seems like you haven&apos;t found your perfect
+                            gear yet. Let&apos;s fix that!
+                        </p>
+                    </div>
 
-                <tbody>
-                    {cartItems.map((e) => (
-                        <TableRow key={e.product_id} {...e} />
-                    ))}
-                </tbody>
-            </table>
+                    <ArrowSvg />
+
+                    <div className="absolute left-[60%] top-[275%] w-full max-[670px]:relative max-[670px]:left-0 max-[670px]:top-0">
+                        <p className="text-2xl text-neutral-400">
+                            Explore our{' '}
+                            <a
+                                href="/catalog"
+                                className="text-orange-500 underline transition-colors duration-300 hover:text-orange-700"
+                            >
+                                collection
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
