f91af9bfd6917a9079b73289638c82ef93288333
PetarIvanov01 <123456789petar45@gmail.com>
2024-03-31 14:21:04 +0300

    Refactor AsideNav component to manage active state

--- src/components/ProfileSection/AsideNav/AsideNav.jsx
+++ src/components/ProfileSection/AsideNav/AsideNav.jsx
@@ -6,30 +6,63 @@ import { IoMdInformationCircleOutline } from 'react-icons/io';
 
 import ProfilePic from './components/ProfilePic';
 import { NavLi } from './components/NavLi';
+import { useState } from 'react';
 
 export default function AsideNav({ userId }) {
+    const [active, setActive] = useState('info');
+
+    const onNavigateSetActive = (id) => {
+        return () => {
+            setActive(id);
+        };
+    };
+
     return (
         <div className="flex w-1/3 min-w-[220px] max-w-[250px] flex-col gap-2 bg-[#1B263E] bg-opacity-65">
             <ProfilePic />
 
             <nav className="h-full rounded-b-md border-2 border-black bg-[#1B263E] bg-opacity-65 text-white">
                 <ul className="flex flex-col justify-center gap-3 px-2 pt-4">
-                    <NavLi Icon={IoMdInformationCircleOutline}>
-                        <Link to={`/profile/info/${userId}`}>
+                    <NavLi
+                        id={'info'}
+                        active={active}
+                        Icon={IoMdInformationCircleOutline}
+                    >
+                        <Link
+                            onClick={onNavigateSetActive('info')}
+                            to={`/profile/info/${userId}`}
+                        >
                             Personal Information
                         </Link>
                     </NavLi>
 
-                    <NavLi Icon={LuMapPin}>
-                        <Link to={`/profile/address/${userId}`}>Addreses</Link>
+                    <NavLi id={'address'} active={active} Icon={LuMapPin}>
+                        <Link
+                            onClick={onNavigateSetActive('address')}
+                            to={`/profile/address/${userId}`}
+                        >
+                            Addreses
+                        </Link>
                     </NavLi>
 
-                    <NavLi Icon={IoTicketOutline}>
-                        <Link to={`/profile/orders/${userId}`}>My Orders</Link>
+                    <NavLi id={'orders'} active={active} Icon={IoTicketOutline}>
+                        <Link
+                            onClick={onNavigateSetActive('orders')}
+                            to={`/profile/orders/${userId}`}
+                        >
+                            My Orders
+                        </Link>
                     </NavLi>
 
-                    <NavLi Icon={MdOutlinePayment}>
-                        <Link to={`/profile/pay-methods/${userId}`}>
+                    <NavLi
+                        id={'pay-methods'}
+                        active={active}
+                        Icon={MdOutlinePayment}
+                    >
+                        <Link
+                            onClick={onNavigateSetActive('pay-methods')}
+                            to={`/profile/pay-methods/${userId}`}
+                        >
                             Payment Methods
                         </Link>
                     </NavLi>
--- src/components/ProfileSection/AsideNav/components/NavLi.jsx
+++ src/components/ProfileSection/AsideNav/components/NavLi.jsx
@@ -1,15 +1,23 @@
 import { MdKeyboardArrowRight } from 'react-icons/md';
 
-export function NavLi({ children, Icon }) {
+export function NavLi({ children, Icon, active, id }) {
+    const activeStyle =
+        active === id
+            ? 'bg-gradient-to-r from-[#5D6890] via-[#4E5C80] to-[#3F4D70] '
+            : '';
+
+    const activeArr = active === id ? 'visible' : 'invisible';
     return (
         <li>
             <div
-                className="group flex items-center gap-1 rounded-md
-          px-0.5 py-1 text-sm hover:bg-gradient-to-r hover:from-[#5D6890] hover:via-[#4E5C80] hover:to-[#3F4D70]"
+                className={`group flex items-center ${activeStyle} gap-1 rounded-md
+          px-0.5 py-1 text-sm hover:bg-gradient-to-r hover:from-[#5D6890] hover:via-[#4E5C80] hover:to-[#3F4D70]`}
             >
                 <Icon className="size-8" />
                 {children}
-                <MdKeyboardArrowRight className="invisible group-hover:visible" />
+                <MdKeyboardArrowRight
+                    className={`${activeArr}  group-hover:visible `}
+                />
             </div>
         </li>
     );
