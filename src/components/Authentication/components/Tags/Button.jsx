5122bfaeb51e2ee1aeb7e24deac8a24a48182368
PetarIvanov01 <123456789petar45@gmail.com>
2024-03-31 14:13:55 +0300

    update Button component with loading spinner

--- src/components/Authentication/components/Tags/Button.jsx
+++ src/components/Authentication/components/Tags/Button.jsx
@@ -1,15 +1,35 @@
 import { GrLinkNext } from 'react-icons/gr';
 
-export default function Button({ text, errors }) {
+export default function Button({ text, errors, loadSpin = false }) {
     const isDisabled = Object.keys(errors).length > 2;
     return (
         <button
             disabled={isDisabled}
-            className="mt-2 flex w-fit items-center gap-2 rounded-sm 
-                 bg-white px-1 py-1 text-black opacity-65 hover:scale-105 hover:bg-blue-400"
+            className="mt-2 flex h-10 w-fit items-center gap-2 rounded-sm
+                 bg-white px-2 py-1 text-black opacity-65 hover:scale-105 hover:bg-blue-400"
             type="submit"
         >
-            {text} <GrLinkNext />
+            <div className="flex w-full items-center justify-center gap-1">
+                <p className="w-fit text-nowrap">{text}</p> <GrLinkNext />
+            </div>
+            {loadSpin && (
+                <div className="w-10">
+                    <svg
+                        viewBox="0 0 800 800"
+                        className="h-10 w-auto animate-spin"
+                    >
+                        <circle
+                            cx="400"
+                            cy="400"
+                            fill="none"
+                            r="200"
+                            strokeWidth="50"
+                            stroke="#3291df"
+                            strokeDasharray="700 1400"
+                        />
+                    </svg>
+                </div>
+            )}
         </button>
     );
 }
