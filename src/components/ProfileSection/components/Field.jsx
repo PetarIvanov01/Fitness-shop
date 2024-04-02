167a1504db961ccc3b49dcd05c2a90831dd90371
PetarIvanov01 <123456789petar45@gmail.com>
2024-04-02 18:44:09 +0300

    update - enhance Field component with lock/unlock icons

--- src/components/ProfileSection/components/Field.jsx
+++ src/components/ProfileSection/components/Field.jsx
@@ -1,3 +1,5 @@
+import { GiPadlock } from 'react-icons/gi';
+import { CiUnlock } from 'react-icons/ci';
 export default function Field({
     value,
     handleOnChange,
@@ -7,10 +9,16 @@ export default function Field({
     type,
     isEdit = false,
 }) {
+    const editStyles = isEdit ? 'text-blue-300' : 'text-[#b9d2f3]';
     return (
         <>
-            <label htmlFor={name} className="text-white">
-                {label}
+            <label htmlFor={name} className={`flex gap-1 ${editStyles}`}>
+                {label}{' '}
+                {isEdit ? (
+                    <GiPadlock color="#93c5fd" />
+                ) : (
+                    <CiUnlock color="#b9d2f3" />
+                )}
             </label>
             <input
                 disabled={isEdit}
