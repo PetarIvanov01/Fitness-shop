959b263f2c01120740b9d8f8c7277b4b2333c97e
PetarIvanov01 <123456789petar45@gmail.com>
2024-04-13 22:58:29 +0300

    refactor  - FinishOrder component to handle the submission of the order data and display a confirmation modal.  - Incorporated state management using useState and useStore hooks for managing consent and clearing the cart item.  - Added error handling for order creation and navigation. Implemented a confirmation modal to display the privacy policy information and handle user consent.

--- src/components/Checkout/components/Order/FinishOrder.jsx
+++ src/components/Checkout/components/Order/FinishOrder.jsx
@@ -1,20 +1,68 @@
+import useStore from '../../../../zustand/store';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+import delay from '../../../../utils/withDelayPromise';
+import { createOrder } from '../../../../api/services/userService/orders';
+
+import ConfirmModal from '../../../ProfileSection/components/ConfirmModal';
 import ReceiveEmails from './components/ReceiveEmails';
 
-export default function FinishOrder() {
+export default function FinishOrder({ orderData }) {
+    const navigate = useNavigate();
+    const [consentGiven, setConsentGiven] = useState(false);
+    const clearCartItem = useStore((state) => state.clearCartItem);
+
+    const handleSubmitConsent = () => {
+        setConsentGiven(false);
+        try {
+            delay(800).then(async () => {
+                await createOrder(orderData.orderInfo._userId, orderData);
+                clearCartItem();
+                navigate('/', { replace: true });
+            });
+        } catch (error) {
+            console.log(error);
+            navigate('/', { replace: true });
+        }
+    };
+
+    const handleModalClose = () => {
+        setConsentGiven(false);
+    };
+
+    const handleOpenModal = () => {
+        setConsentGiven(true);
+    };
+
     return (
         <section className="flex flex-wrap justify-between gap-4 pt-6">
-            <div className="w-2/3">
-                <ReceiveEmails />
-                <p className="text-[0.7em] font-extralight">
-                    Your provided information will be used to process your order
-                    and for other purposes outlined in our privacy policy. By
-                    proceeding, you agree to our privacy policy.
-                </p>
-            </div>
+            <ReceiveEmails />
 
-            <button className="h-max w-max border border-[#EDAD09] px-1 text-xl text-[#EDAD09] hover:opacity-60">
+            <button
+                onClick={handleOpenModal}
+                className="h-max w-max border-2  border-[#EDAD09] px-1 text-xl text-[#EDAD09] hover:scale-105 hover:opacity-60"
+            >
                 Complete Purchase
             </button>
+
+            {consentGiven && (
+                <ConfirmModal
+                    onAcceptCloseModal={handleSubmitConsent}
+                    toggleModal={handleModalClose}
+                    text={textForModal}
+                />
+            )}
         </section>
     );
 }
+const textForModal = (
+    <>
+        Your provided information will be used to process your order and for
+        other purposes outlined in our{' '}
+        <Link to="/privacy-policy" className="text-blue-500 hover:underline">
+            privacy policy
+        </Link>
+        . By proceeding, you agree to our privacy policy.
+    </>
+);
