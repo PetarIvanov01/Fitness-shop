bbf8498904bfe3c6144fa34563f90fd608007c90
PetarIvanov01 <123456789petar45@gmail.com>
2024-04-13 22:56:17 +0300

    refactor - OrderSection component to improve reusability and readability. - Incorporated a separate OrderTable component to handle rendering of the table content, making the code more modular. - Added memoization to enhance performance by preventing unnecessary re-renders when the props remain unchanged.

--- src/components/Checkout/components/Order/OrderSection.jsx
+++ src/components/Checkout/components/Order/OrderSection.jsx
@@ -1,11 +1,11 @@
-import { TbClipboardList } from 'react-icons/tb';
-import { useEffect, useState } from 'react';
+import { memo, useEffect, useState } from 'react';
 import useStore from '../../../../zustand/store';
 
-import TableRow from './components/TableRow';
+import { TbClipboardList } from 'react-icons/tb';
 import FinishOrder from './FinishOrder';
+import OrderTable from './components/OrderTable';
 
-export default function OrderSection() {
+const OrderSection = memo(function OrderSection({ orderData }) {
     const fetchCartData = useStore((state) => state.fetchCartData);
     const [cart, setCart] = useState([]);
 
@@ -26,6 +26,13 @@ export default function OrderSection() {
                   return prev + Number(curr.price) * Number(curr.quantity);
               }, 0);
 
+    orderData.orderInfo.totalPrice = Math.round(totalPrice * 100) / 100;
+    orderData.orderProducts = cart.map((e) => ({
+        _productId: e.product_id,
+        quantity: e.quantity,
+        subtotal: Number(e.price),
+    }));
+
     return (
         <section className="px-4 pb-14 pt-6 text-white">
             <header className="flex items-center gap-3 pb-5">
@@ -33,64 +40,11 @@ export default function OrderSection() {
                 <TbClipboardList size={35} />
             </header>
 
-            <div className="w-full  text-white">
-                <table className="w-full text-left">
-                    <thead className="border max-sm:text-[0.7em]">
-                        <tr>
-                            <th className="p-2">Product</th>
-                            <th className="p-2">Subtotal</th>
-                        </tr>
-                    </thead>
-                    <tbody className="text-left">
-                        {cart.length > 0 &&
-                            cart.map((order) => {
-                                const orderInfo = {
-                                    quantity: order.quantity,
-                                    name: order.title,
-                                    subtotal:
-                                        Number(order.price) *
-                                        Number(order.quantity),
-                                };
-                                return (
-                                    <TableRow
-                                        key={order.product_id}
-                                        {...orderInfo}
-                                    />
-                                );
-                            })}
-                        <tr className="border text-left max-sm:text-[0.7em]">
-                            <td className="p-2 font-semibold">
-                                <span>Subtotal:</span>
-                            </td>
-                            <td className="p-2 ">
-                                <span className="">$ 3232.23</span>
-                            </td>
-                        </tr>
+            <OrderTable cart={cart} totalPrice={totalPrice} />
 
-                        <tr className="border max-sm:text-[0.7em]">
-                            <td className="p-2 font-semibold">
-                                <span>Shipping:</span>
-                            </td>
-                            <td className="p-2 hover:cursor-pointer hover:underline">
-                                <span className="">$ 25</span>
-                            </td>
-                        </tr>
-
-                        <tr className="border max-sm:text-[0.7em]">
-                            <td className="p-2 font-bold">
-                                <span>Total:</span>
-                            </td>
-                            <td className="p-2 hover:cursor-pointer hover:underline">
-                                <span className="font-semibold">
-                                    $ {totalPrice.toFixed(2)}
-                                </span>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-
-            <FinishOrder />
+            <FinishOrder orderData={orderData} />
         </section>
     );
-}
+});
+
+export default OrderSection;
