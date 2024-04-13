f3c69d059a6cf016034cae913b947a7955322bf6
PetarIvanov01 <123456789petar45@gmail.com>
2024-04-13 22:52:09 +0300

    update: - ProfileView component to include the type property from the context object, allowing for more flexible rendering based on the type of profile being viewed.

--- src/components/ProfileSection/ProfileView.jsx
+++ src/components/ProfileSection/ProfileView.jsx
@@ -4,15 +4,16 @@ import AsideNav from './AsideNav/AsideNav';
 export default function ProfileView() {
     const context = useOutletContext();
     const userId = context.userId;
+    const type = context.type;
 
     const RenderedSection = context.RenderedSection;
 
     return (
         <section className="flex min-h-[800px] w-full justify-center bg-slate-900 bg-opacity-95 font-inter">
-            <div className="flex w-full justify-center gap-4 p-2 max-ml:flex-col max-ml:items-center max-ml:justify-normal">
-                <AsideNav userId={userId} />
+            <div className="flex w-4/5 justify-center gap-4 p-2 max-ml:flex-col max-ml:items-center max-ml:justify-normal">
+                <AsideNav userId={userId} type={type} />
 
-                <section className="w-full max-w-[560px] rounded-md border-2 border-black bg-[#1B263E] bg-opacity-65">
+                <section className="w-full rounded-md border-2 border-black bg-[#1B263E] bg-opacity-65">
                     <RenderedSection userId={context.userId} />
                 </section>
             </div>
