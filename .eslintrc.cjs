24a89a6be0b5e55a8485ebd996cb9669cdc2f6b0
PetarIvanov01 <123456789petar45@gmail.com>
2024-04-21 18:49:09 +0300

    ignore build and cypress folders

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,7 +11,7 @@ module.exports = {
         'plugin:react/jsx-runtime',
         'plugin:react-hooks/recommended',
     ],
-    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    ignorePatterns: ['build', '.eslintrc.cjs', 'cypress*'],
     parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
     settings: { react: { version: '18.2' } },
     plugins: ['react-refresh', 'cypress'],
