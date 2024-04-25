dd165e4bc0536878a53438c0b87abf1ab5d1526d
PetarIvanov01 <123456789petar45@gmail.com>
2024-04-25 19:19:30 +0300

    add vite-plugin-checker to show type erros in the browser

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import tsCheck from 'vite-plugin-checker';
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
         outDir: 'build',
     },
-    plugins: [react()],
+    plugins: [react(), tsCheck({ typescript: true })],
 });
