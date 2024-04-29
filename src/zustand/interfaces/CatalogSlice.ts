e5521f04391987a96876e9ac69241550d924048f
PetarIvanov01 <123456789petar45@gmail.com>
2024-04-29 22:22:09 +0300

    refactor: - Created a type `FetchCatalogType` to specify the signature of the `callCatalogSetStore` function. - Defined a type `CatalogResultType` to represent the structure of the catalog result data.

--- src/zustand/interfaces/CatalogSlice.ts
+++ src/zustand/interfaces/CatalogSlice.ts
@@ -1,7 +1,20 @@
+type FetchCatalogType = (
+    querieString: string,
+    signal: AbortSignal
+) => Promise<CatalogResultType>;
+
+export type CatalogResultType = {
+    product_id: string;
+    categoriy_id: number;
+    title: string;
+    price: string;
+    description: string;
+    image: string;
+    quantity: string;
+    type: string;
+}[];
+
 export interface CatalogSliceInter {
     itemsLngInDb: number;
-    callCatalogSetStore: (
-        querieString: string,
-        signal: AbortSignal
-    ) => Promise<any>;
+    callCatalogSetStore: FetchCatalogType;
 }
