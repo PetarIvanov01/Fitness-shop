edfb9b0f1b1b7d7ca12d1fe2253348845b9e268b
PetarIvanov01 <123456789petar45@gmail.com>
2024-04-30 19:47:31 +0300

    refactor CatalogLink component - Added CatalogParams types for type safety

--- src/components/CatalogLink.tsx
+++ src/components/CatalogLink.tsx
@@ -3,24 +3,23 @@ import { Link } from 'react-router-dom';
 const path = '/catalog';
 const optionQueries = {
     default: `${path}?from=0&to=10000&sort_by=asc`,
-    category: (val) => `${path}?from=0&to=10000&category=${val}&sort_by=asc`,
+    category: (val: string) =>
+        `${path}?from=0&to=10000&category=${val}&sort_by=asc`,
 };
 
-/**
- * CatalogLink defining props with types
- * @param {Object} props
- * @param {('default' | 'category')} props.queryType
- * @param {string} props.queryValue
- * @param {Function} props.handler
- * @returns {JSX.Element}
- */
+type CatalogParams = {
+    queryType?: 'default' | 'category';
+    queryValue?: string;
+    children: JSX.Element;
+    handler?: () => void;
+};
 
 export default function CatalogLink({
     queryType = 'default',
     queryValue = '',
     children,
     handler = () => {},
-}) {
+}: CatalogParams) {
     const type = optionQueries[queryType];
 
     let endpoint = '';
