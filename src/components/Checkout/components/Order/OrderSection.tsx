b418a70a65d14ab90eed7aeb6b311a4590cad3d2
PetarIvanov01 <123456789petar45@gmail.com>
2024-04-30 19:43:05 +0300

    refactor OrderSection Component - Defined a Props type to specify the expected structure of the props passed to OrderSection.
    - Included ProductInCart type to ensure type safety for the cart state.
    
    -Modified finishedOrderData to include type-safe properties based on the Props type.

--- src/components/Checkout/components/Order/OrderSection.tsx
+++ src/components/Checkout/components/Order/OrderSection.tsx
@@ -1,13 +1,29 @@
 import { memo, useEffect, useState } from 'react';
+import { ProductInCart } from '../../../../zustand/interfaces/CartSlice';
+
 import useStore from '../../../../zustand/store';
 
-import { TbClipboardList } from 'react-icons/tb';
 import FinishOrder from './FinishOrder';
 import OrderTable from './components/OrderTable';
+import { TbClipboardList } from 'react-icons/tb';
 
-const OrderSection = memo(function OrderSection({ orderData }) {
+type Props = {
+    orderData: {
+        orderInfo: {
+            _userId: string;
+            orderAddress: {
+                address: string;
+                country: string;
+                city: string;
+                postcode: number;
+            };
+        };
+    };
+};
+
+const OrderSection = memo(function OrderSection({ orderData }: Props) {
     const fetchCartData = useStore((state) => state.fetchCartData);
-    const [cart, setCart] = useState([]);
+    const [cart, setCart] = useState<ProductInCart[]>([]);
 
     useEffect(() => {
         const abortController = new AbortController();
@@ -26,12 +42,18 @@ const OrderSection = memo(function OrderSection({ orderData }) {
                   return prev + Number(curr.price) * Number(curr.quantity);
               }, 0);
 
-    orderData.orderInfo.totalPrice = Math.round(totalPrice * 100) / 100;
-    orderData.orderProducts = cart.map((e) => ({
-        _productId: e.product_id,
-        quantity: e.quantity,
-        subtotal: Number(e.price),
-    }));
+    const finishedOrderData = {
+        ...orderData,
+        orderInfo: {
+            ...orderData.orderInfo,
+            totalPrice: Math.round(totalPrice * 100) / 100,
+        },
+        orderProducts: cart.map((e) => ({
+            _productId: e.product_id,
+            quantity: e.quantity,
+            subtotal: Number(e.price),
+        })),
+    };
 
     return (
         <section className="px-4 pb-14 pt-6 text-white">
@@ -42,7 +64,7 @@ const OrderSection = memo(function OrderSection({ orderData }) {
 
             <OrderTable cart={cart} totalPrice={totalPrice} />
 
-            <FinishOrder orderData={orderData} />
+            <FinishOrder finishedOrderData={finishedOrderData} />
         </section>
     );
 });
