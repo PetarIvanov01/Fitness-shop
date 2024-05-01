b19987775b578fdff70785317354ff0cde7d060b
PetarIvanov01 <123456789petar45@gmail.com>
2024-05-01 23:44:22 +0300

    refactor: - createQueryString to use TypeScript generics for improved type safety

--- src/utils/createQueryString.ts
+++ src/utils/createQueryString.ts
@@ -1,9 +1,18 @@
-export default function createQueryString(queryObj) {
-    const string = Object.keys(queryObj)
+type ObjectValue = number | string | boolean;
+
+type GeneralObject<T> = { [K in keyof T]: ObjectValue };
+
+export default function createQueryString<T extends GeneralObject<T>>(
+    queryObj: T
+) {
+    const keys = Object.keys(queryObj) as (keyof T)[];
+
+    const string = keys
         .filter((key) => queryObj[key] !== null)
         .map(
             (key) =>
-                `${encodeURIComponent(key)}=${encodeURIComponent(queryObj[key])}`
+                //@ts-ignore
+                `${encodeURIComponent(key)}=${encodeURIComponent(queryObj[key as keyof T])}`
         )
         .join('&');
     return string;
