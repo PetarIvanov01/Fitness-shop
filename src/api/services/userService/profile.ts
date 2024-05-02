fdd32050911e471ae644b26e7ec03034f1e34120
PetarIvanov01 <123456789petar45@gmail.com>
2024-05-02 17:20:02 +0300

    remove user.ts file and type safety to getUserInformation fucntion

--- src/api/services/userService/profile.ts
+++ src/api/services/userService/profile.ts
@@ -1,14 +1,18 @@
 import * as methods from '../../requester';
 
+import { PersonalInfoReturnedType } from '../../../zustand/interfaces/UserSlice';
+
 const endpoints = {
     getUser: (userId: string) => `/users/${userId}`,
     updateUser: (userId: string) => `/users/${userId}`,
 };
+
 export const getUserInformation = async (
     userId: string,
     signal: AbortSignal
-) => {
-    return await methods.get(endpoints.getUser(userId), null, signal);
+): Promise<PersonalInfoReturnedType> => {
+    const res = await methods.get(endpoints.getUser(userId), null, signal);
+    return res;
 };
 
 export const updateUserInformation = async (
