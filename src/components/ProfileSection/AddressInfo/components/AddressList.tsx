912fd743bbe25058e1c55d4b60cc40e26abe2255
PetarIvanov01 <123456789petar45@gmail.com>
2024-11-05 14:13:38 +0200

    refactor: remove using global store and add todo

--- src/components/ProfileSection/AddressInfo/components/AddressList.tsx
+++ src/components/ProfileSection/AddressInfo/components/AddressList.tsx
@@ -1,13 +1,16 @@
-import useStore from '../../../../zustand/store';
 import AddressBox from './AddressBox';
 
 export default function AddressList() {
-    const addresses = useStore((state) => state.otherShippingAddresses);
+    const addresses: any = [];
+
+    // Todo: fetch other addresses
 
     return (
         <div className="flex flex-col gap-6 pb-5 max-s:text-[0.8em]">
             {addresses.length > 0 ? (
-                addresses.map((e) => <AddressBox {...e} key={e.address_id} />)
+                addresses.map((e: any) => (
+                    <AddressBox {...e} key={e.address_id} />
+                ))
             ) : (
                 <div className="h-full px-1 text-center text-[1.2em]">
                     <p className="text-gray-400 max-s:text-[1em]">
