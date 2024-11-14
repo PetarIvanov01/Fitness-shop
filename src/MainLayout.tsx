9399cdc93cdf9a25f68c73e8fb6c9f5d14dcad75
PetarIvanov01 <123456789petar45@gmail.com>
2024-11-14 13:06:43 +0200

    chore: rename the component

--- src/MainLayout.tsx
+++ src/MainLayout.tsx
@@ -4,9 +4,9 @@ import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import Header from './components/Headers/MainHeader';
 
-export default function App() {
+export default function MainLayout() {
     return (
-        <div className="flex flex-col bg-gray-500 bg-fitnes-img bg-cover bg-center bg-no-repeat object-contain bg-blend-multiply">
+        <div className="flex min-h-screen flex-col bg-gray-500 bg-fitnes-img bg-cover bg-center bg-no-repeat object-contain bg-blend-multiply">
             <Header />
 
             <div className="flex min-h-screen flex-col py-8">
